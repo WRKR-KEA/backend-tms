4cd899399202c0bbb0bde2dba37696780a994dc6
jiwon <kiwijomn@gmail.com>
2025-01-18 15:57:32 +0900

    🎉 프로젝트 생성

---
+++ src/test/java/com/wrkr/tickety/TicketyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrkr.tickety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
