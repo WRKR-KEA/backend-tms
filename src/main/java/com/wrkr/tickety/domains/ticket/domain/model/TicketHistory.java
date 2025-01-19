f4e3fdd5b0cc31ed2ee79fc18d4133da279387a9
jiwon <kiwijomn@gmail.com>
2025-01-19 10:47:26 +0900

    WRKR-57 :sparkles: Feature: TicketHistory Entity 생성
    <footer>
    #6

---
+++ src/main/java/com/wrkr/tickety/domains/ticket/domain/model/TicketHistory.java
@@ -0,0 +1,48 @@
+package com.wrkr.tickety.domains.ticket.domain.model;
+
+import com.wrkr.tickety.domains.member.domain.model.Member;
+import com.wrkr.tickety.domains.ticket.domain.constant.ModifiedType;
+import com.wrkr.tickety.domains.ticket.domain.constant.TicketStatus;
+import com.wrkr.tickety.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
+@Table(name = "ticket_history")
+public class TicketHistory extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ticketHistoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id", nullable = false)
+    private Member manager;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TicketStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ModifiedType modified;
+
+    @Builder
+    public TicketHistory(Ticket ticket, Member manager, TicketStatus status, ModifiedType modified) {
+        this.ticket = ticket;
+        this.manager = manager;
+        this.status = status;
+        this.modified = modified;
+    }
+}
