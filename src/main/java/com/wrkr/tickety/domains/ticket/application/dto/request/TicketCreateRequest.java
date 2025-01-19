68f9e1ee601a4e9c1309d7b362d4ee1911471e85
PARK SEONWOONG <sunwoong0169@naver.com>
2025-01-19 21:33:24 +0900

    WRKR-62 ✨ Feature: 티켓 생성 요청 DTO 구현

---
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/dto/request/TicketCreateRequest.java
@@ -0,0 +1,22 @@
+package com.wrkr.tickety.domains.ticket.application.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record TicketCreateRequest(
+
+        @Schema(description = "티켓 제목", example = "티켓 제목")
+        @NotBlank(message = "티켓 제목은 필수 항목입니다.")
+        String title,
+
+        @Schema(description = "티켓 내용", example = "티켓 내용")
+        @NotBlank(message = "티켓 내용은 필수 항목입니다.")
+        String content,
+
+        @Schema(description = "2차 카테고리", example = "1")
+        @NotNull(message = "2차 카테고리는 필수 항목입니다.")
+        Long categoryId
+
+) {
+}
