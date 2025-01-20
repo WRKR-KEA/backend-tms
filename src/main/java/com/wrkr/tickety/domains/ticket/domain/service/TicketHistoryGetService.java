9c52fc7cb30972f330b6dd038273c71fd9771ce7
PARK SEONWOONG <sunwoong0169@naver.com>
2025-01-21 01:06:50 +0900

    WRKR-62 ✨ Feature: 티켓 변경내역 관련 로직 구현

---
+++ src/main/java/com/wrkr/tickety/domains/ticket/domain/service/TicketHistoryGetService.java
@@ -0,0 +1,26 @@
+package com.wrkr.tickety.domains.ticket.domain.service;
+
+import com.wrkr.tickety.domains.ticket.domain.constant.ModifiedType;
+import com.wrkr.tickety.domains.ticket.domain.model.TicketHistory;
+import com.wrkr.tickety.domains.ticket.persistence.repository.TicketHistoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class TicketHistoryGetService {
+
+    private final TicketHistoryRepository ticketHistoryRepository;
+
+
+    public LocalDateTime getFirstManagerChangeDate(Long ticketId) {
+        return ticketHistoryRepository.findFirstByTicket_TicketIdAndModifiedOrderByCreatedAtAsc(
+                ticketId,
+                ModifiedType.MANAGER
+        ).map(TicketHistory::getCreatedAt).orElse(null);
+    }
+}
