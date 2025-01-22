bc7fcd00ef17365cfa42cb5520035a942efa46ee
strongmhk <kim6562166086@gmail.com>
2025-01-22 13:42:03 +0900

    WRKR-65 ✨ Feature: DTO 설명 추가

--- src/main/java/com/wrkr/tickety/domains/member/application/dto/request/MemberCreateRequest.java
+++ src/main/java/com/wrkr/tickety/domains/member/application/dto/request/MemberCreateRequest.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
+@Schema(description = "회원 생성 요청 DTO")
 @Builder
 public record MemberCreateRequest(
 
@@ -15,21 +16,31 @@ public record MemberCreateRequest(
         @EmailFormat
         String email,
 
+        @Schema(description = "이름", example = "김가천")
         @NotBlank(message = "이름은 공백일 수 없습니다.")
         String name,
 
+        @Schema(description = "닉네임", example = "gachon.km")
         @NotBlank(message = "닉네임은 공백일 수 없습니다.")
         String nickname,
 
+        @Schema(description = "부서", example = "백엔드 개발팀")
+        @NotBlank(message = "부서는 공백일 수 없습니다.")
+        String department,
+
+        @Schema(description = "직책", example = "팀장")
         @NotBlank(message = "직책은 공백일 수 없습니다.")
         String position,
 
+        @Schema(description = "전화번호", example = "010-1234-5678")
         @NotBlank(message = "전화번호는 공백일 수 없습니다.")
         @PhoneNumberFormat
         String phone,
 
+        @Schema(description = "권한", example = "사용자")
         Role role,
 
+        @Schema(description = "프로필 이미지 URL", example = "https://ibb.co/Gt8fycB")
         @NotBlank(message = "프로필 이미지는 공백일 수 없습니다.")
         String profileImage
 ) {
