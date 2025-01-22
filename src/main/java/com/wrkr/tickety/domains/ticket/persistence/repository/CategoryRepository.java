7ddaa38935eeb02cec6e670599637f68848c9d67
jiwon <kiwijomn@gmail.com>
2025-01-22 15:15:48 +0900

    WRKR-79 :recycle: Refactor: Repository 레이어 Entity 클래스 변경
    <footer>
    #39

--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/CategoryRepository.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/CategoryRepository.java
@@ -1,8 +1,9 @@
 package com.wrkr.tickety.domains.ticket.persistence.repository;
 
 import com.wrkr.tickety.domains.ticket.domain.model.Category;
+import com.wrkr.tickety.domains.ticket.persistence.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
     List<Category> findByIsDeletedFalse();
 }
--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/GuideRepository.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/GuideRepository.java
@@ -1,11 +1,12 @@
 package com.wrkr.tickety.domains.ticket.persistence.repository;
 
 import com.wrkr.tickety.domains.ticket.domain.model.Guide;
+import com.wrkr.tickety.domains.ticket.persistence.entity.GuideEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface GuideRepository extends JpaRepository<Guide, Long> {
+public interface GuideRepository extends JpaRepository<GuideEntity, Long> {
     Optional<Guide> findByCategory_CategoryId(Long category_id);
 
     Boolean existsByCategory_CategoryId(Long categoryId);
