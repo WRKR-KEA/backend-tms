f827d6c3340a0bb65928a795b027722b789cd0a0
hurrayPersimmon <rahel337@naver.com>
2025-01-23 05:06:17 +0900

    WRKR-82 :bug: Fix: 카테고리 에러코드 변경으로 인한 에러 수정

--- src/main/java/com/wrkr/tickety/domains/ticket/application/usecase/guide/GuideCreateUseCase.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/usecase/guide/GuideCreateUseCase.java
@@ -23,7 +23,7 @@ public class GuideCreateUseCase {
         //todo 카테고리 도메인이 아직 없어서 카테고리 도메인이 생기면 dto <-> 카테고리 도메인 mapper에서 암호화/복호화 로직을 처리하는 방향으로 수정 예정
 
         Category category = categoryGetService.getCategory(guideCreateRequest.categoryId())
-                .orElseThrow(() -> ApplicationException.from(CategoryErrorCode.CATEGORY_NOT_FOUND));
+                .orElseThrow(() -> ApplicationException.from(CategoryErrorCode.CATEGORY_NOT_EXIST));
 
         Guide guide = Guide.builder()
                 .content(guideCreateRequest.content())
