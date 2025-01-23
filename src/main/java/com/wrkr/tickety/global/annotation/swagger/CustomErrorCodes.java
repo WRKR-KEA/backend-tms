a1874a51d0a6e012427b70116728913dc6724f9c
strongmhk <kim6562166086@gmail.com>
2025-01-23 10:24:08 +0900

    WRKR-65 🐛 Fix: 오류 주석 처리

--- src/main/java/com/wrkr/tickety/global/annotation/swagger/CustomErrorCodes.java
+++ src/main/java/com/wrkr/tickety/global/annotation/swagger/CustomErrorCodes.java
@@ -16,6 +16,6 @@ public @interface CustomErrorCodes {
 
     CommonErrorCode[] commonErrorCodes() default {};
 
-    MemberErrorCode[] value();
+//    MemberErrorCode[] value();
     GuideErrorCode[] value();
 }
