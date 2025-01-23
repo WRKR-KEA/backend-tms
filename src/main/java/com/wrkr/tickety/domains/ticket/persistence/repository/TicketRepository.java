69492876ae6cb2cffb97d89adc4a73325195d241
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-01-23 10:10:08 +0900

    WRKR-81 ✨ Feature: 담당자가 설정한 필터링으로 티켓 목록 가져오는 쿼리 추가

--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketRepository.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketRepository.java
@@ -1,6 +1,6 @@
 package com.wrkr.tickety.domains.ticket.persistence.repository;
 
-import com.wrkr.tickety.domains.ticket.domain.model.Ticket;
+import com.wrkr.tickety.domains.ticket.domain.constant.TicketStatus;
 import com.wrkr.tickety.domains.ticket.persistence.entity.TicketEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +10,20 @@ import org.springframework.data.repository.query.Param;
 
 public interface TicketRepository extends JpaRepository<TicketEntity, Long> {
 
-    @Query("SELECT t FROM TicketEntity t WHERE t.user.memberId = :userId" )
+    @Query("SELECT t FROM TicketEntity t WHERE t.user.memberId = :userId")
     Page<TicketEntity> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("""
+                SELECT t
+                FROM TicketEntity t
+                WHERE t.manager.memberId = :managerId
+                  AND (:status IS NULL OR t.status = :status)
+                ORDER BY CASE WHEN :isPinned = TRUE THEN t.isPinned END DESC, t.ticketId ASC
+            """)
+    Page<TicketEntity> findByManagerFilters(
+            @Param("managerId") Long managerId,
+            @Param("status") TicketStatus status,
+            @Param("isPinned") Boolean isPinned,
+            Pageable pageable
+    );
 }
