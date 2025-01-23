74d122dee23b95671d3f0a855c89d499e5f0b683
hurrayPersimmon <rahel337@naver.com>
2025-01-23 15:51:41 +0900

    WRKR-84 :recycle: Refactor: PkResponse.java @Builder 추가

--- src/main/java/com/wrkr/tickety/domains/ticket/application/dto/response/PkResponse.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/dto/response/PkResponse.java
@@ -1,8 +1,12 @@
 package com.wrkr.tickety.domains.ticket.application.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
+@Builder
+@Schema(description = "해당 API 주체를 담은 dto", name = "PkResponse")
 public record PkResponse(
+
         @Schema(description = "해당 API 주체의 ID", example = "Bqs3C822lkMNdWlmE-szUw")
         String id
 ) {
