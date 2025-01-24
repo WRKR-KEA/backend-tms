baac8af3c31d4a946be2ebf48c4a71c94025a8be
strongmhk <kim6562166086@gmail.com>
2025-01-24 14:23:03 +0900

    WRKR-85 ✨ Feature: 회원 정보 페이징 Presentation Laye
    <footer>
    #52

--- src/main/java/com/wrkr/tickety/domains/member/presentation/AdminMemberController.java
+++ src/main/java/com/wrkr/tickety/domains/member/presentation/AdminMemberController.java
@@ -3,26 +3,34 @@ package com.wrkr.tickety.domains.member.presentation;
 import com.wrkr.tickety.domains.member.application.dto.request.MemberCreateRequest;
 import com.wrkr.tickety.domains.member.application.dto.request.MemberDeleteRequest;
 import com.wrkr.tickety.domains.member.application.dto.request.MemberUpdateRequest;
+import com.wrkr.tickety.domains.member.application.dto.response.MemberInfoPagingResponse;
 import com.wrkr.tickety.domains.member.application.dto.response.MemberPkResponse;
 import com.wrkr.tickety.domains.member.application.dto.response.MemberInfoResponse;
 import com.wrkr.tickety.domains.member.application.usecase.MemberCreateUseCase;
-import com.wrkr.tickety.domains.member.application.usecase.MemberGetUseCase;
+import com.wrkr.tickety.domains.member.application.usecase.MemberInfoGetUseCase;
 import com.wrkr.tickety.domains.member.application.usecase.MemberUpdateUseCase;
+import com.wrkr.tickety.domains.member.application.usecase.TotalMemberInfoGetUseCase;
+import com.wrkr.tickety.domains.member.domain.constant.Role;
 import com.wrkr.tickety.global.response.ApplicationResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/members")
 @Tag(name = "Admin Member Controller")
+@Validated
 public class AdminMemberController {
+
     private final MemberCreateUseCase memberCreateUseCase;
     private final MemberUpdateUseCase memberUpdateUseCase;
-    private final MemberGetUseCase memberGetUseCase;
+    private final MemberInfoGetUseCase memberInfoGetUseCase;
+    private final TotalMemberInfoGetUseCase totalMemberInfoGetUseCase;
 
     // TODO: 해당 컨트롤러 모든 API에 헤더 토큰으로 요청한 회원 PK 추출 및 권한이 관리자인지 검증 필요
     // TODO: Image 처리 방식 논의 필요(Multipart or ImageUrl DTO로 바로 받기)
@@ -54,7 +62,16 @@ public class AdminMemberController {
 //    @CustomErrorCodes({MemberErrorCode.MEMBER_NOT_FOUND})
     @GetMapping("/{memberId}")
     public ApplicationResponse<MemberInfoResponse> getMemberInfo(@PathVariable String memberId) {
-        MemberInfoResponse memberInfoDTO = memberGetUseCase.getMemberInfo(memberId);
+        MemberInfoResponse memberInfoDTO = memberInfoGetUseCase.getMemberInfo(memberId);
         return ApplicationResponse.onSuccess(memberInfoDTO);
+    }
+
+    // TODO: ConstraintViolationException 제대로 처리되지 않는 문제 해결 필요
+    @Operation(summary = "관리자 - 회원 정보 목록 조회", description = "전체 회원을 페이징으로 조회합니다. page는 1 이상, size는 10 이상이어야 합니다")
+    @GetMapping
+    public ApplicationResponse<MemberInfoPagingResponse> getTotalMemberInfo(@RequestParam(defaultValue = "0") @Min(value = 1, message = "페이지 번호는 1 이상이어야 합니다.") int page,
+                                                                            @RequestParam(defaultValue = "10") @Min(value = 10, message = "페이지 크기는 10 이상이어야 합니다.") int size,
+                                                                            @RequestParam(required = false) String role) {
+        return ApplicationResponse.onSuccess(totalMemberInfoGetUseCase.getTotalMemberInfo(page - 1, size, role));
     }
 }
