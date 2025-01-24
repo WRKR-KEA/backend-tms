62c1e7298ead9e9822cf235a70068f56d47c314a
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-01-24 20:38:09 +0900

    WRKR-89 ♻️ Refactor: 날짜 검증 정규식 추가

--- src/main/java/com/wrkr/tickety/domains/ticket/application/dto/request/StatisticsByCategoryRequest.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/dto/request/StatisticsByCategoryRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record StatisticsByCategoryRequest(
     @Schema(description = "통계를 확인하고자 하는 날짜", example = "yyyy-mm-dd")
-    @Pattern(regexp = "^(\\d{4}(-\\d{2}(-\\d{2})?)?)$", message = "잘못된 날짜 형식입니다.")
+    @Pattern(regexp = "^(?!\\d{4}-\\d{2}-\\d{2}$).*$", message = "잘못된 날짜 형식입니다.")
     String date
 ) {
 
