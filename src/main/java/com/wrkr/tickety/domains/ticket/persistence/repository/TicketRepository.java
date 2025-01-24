4327c9b385689dd251e2e4e90ed59f2536264e57
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-01-24 20:48:15 +0900

    WRKR-89 ✨ Feature: 날짜 범위 내의 티켓 수 반환 함수 추가

--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketRepository.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketRepository.java
@@ -1,7 +1,7 @@
 package com.wrkr.tickety.domains.ticket.persistence.repository;
 
-import com.wrkr.tickety.domains.ticket.domain.model.Ticket;
 import com.wrkr.tickety.domains.ticket.persistence.entity.TicketEntity;
+import java.time.LocalDate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +10,10 @@ import org.springframework.data.repository.query.Param;
 
 public interface TicketRepository extends JpaRepository<TicketEntity, Long> {
 
-    @Query("SELECT t FROM TicketEntity t WHERE t.user.memberId = :userId" )
+    @Query("SELECT t FROM TicketEntity t WHERE t.user.memberId = :userId")
     Page<TicketEntity> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("SELECT COUNT(t) FROM TicketEntity t WHERE t.category.categoryId = :categoryId AND t.createdAt >= :date AND t.createdAt < :end")
+    Long getTicketCountByCategoryAndDateRange(Long categoryId, LocalDate date, LocalDate end);
+
 }
