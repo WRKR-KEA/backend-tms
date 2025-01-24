afc5f01a6678b854ee05d2f68b5bc22cca68629f
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-01-24 20:25:54 +0900

    WRKR-89 ♻️ Refactor: 스웨거 설명 수정

--- src/main/java/com/wrkr/tickety/domains/ticket/application/dto/request/StatisticsByCategoryRequest.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/dto/request/StatisticsByCategoryRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Pattern;
 
 public record StatisticsByCategoryRequest(
-    @Schema(description = "통계를 확인하고자 하는 날짜", example = "yyyy-mm or yyyym-mm-dd or yyyy")
+    @Schema(description = "통계를 확인하고자 하는 날짜", example = "yyyy-mm-dd")
     @Pattern(regexp = "^(\\d{4}(-\\d{2}(-\\d{2})?)?)$", message = "잘못된 날짜 형식입니다.")
     String date
 ) {
