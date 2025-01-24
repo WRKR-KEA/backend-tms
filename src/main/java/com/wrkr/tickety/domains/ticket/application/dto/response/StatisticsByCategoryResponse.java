9a9cc7ce7e92b85bb64207ce92be2d095915169c
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-01-24 20:20:51 +0900

    WRKR-89 ♻️ Refactor: 응답 dto수정

--- src/main/java/com/wrkr/tickety/domains/ticket/application/dto/response/StatisticsByCategoryResponse.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/dto/response/StatisticsByCategoryResponse.java
@@ -1,32 +1,27 @@
 package com.wrkr.tickety.domains.ticket.application.dto.response;
 
 import java.util.List;
+import lombok.Builder;
 
+@Builder
 public record StatisticsByCategoryResponse(
     String date,
-    String message,
-    Result result
+    StatisticData statisticData
 ) {
 
-    public record Result(
-        List<FirstCategory> firstCategory,
-        List<SecondCategory> secondCategory
+    @Builder
+    public record StatisticData(
+        List<TicketCount> FirstCategoryTicketCount,
+        List<TicketCount> SecondCategoryTicketCount
     ) {
 
     }
 
-    public record FirstCategory(
+    @Builder
+    public record TicketCount(
         String categoryId,
         String categoryName,
-        int count
-    ) {
-
-    }
-
-    public record SecondCategory(
-        String categoryId,
-        String categoryName,
-        int count
+        long count
     ) {
 
     }
