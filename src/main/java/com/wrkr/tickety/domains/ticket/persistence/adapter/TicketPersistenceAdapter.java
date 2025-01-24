5ddd3cbca8b4399b372bbccb1e7afa026247b878
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-01-24 20:47:26 +0900

    WRKR-89 ✨ Feature: 날짜 범위 내의 티켓 수 반환 함수 추가

--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/adapter/TicketPersistenceAdapter.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/adapter/TicketPersistenceAdapter.java
@@ -6,6 +6,7 @@ import com.wrkr.tickety.domains.ticket.persistence.entity.TicketEntity;
 import com.wrkr.tickety.domains.ticket.persistence.mapper.TicketPersistenceMapper;
 import com.wrkr.tickety.domains.ticket.persistence.repository.TicketRepository;
 import com.wrkr.tickety.global.exception.ApplicationException;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,19 +20,28 @@ public class TicketPersistenceAdapter {
     private final TicketPersistenceMapper ticketPersistenceMapper;
 
     public Ticket save(final Ticket ticket) {
+
         TicketEntity ticketEntity = this.ticketPersistenceMapper.toEntity(ticket);
         TicketEntity savedEntity = this.ticketRepository.save(ticketEntity);
         return this.ticketPersistenceMapper.toDomain(savedEntity);
     }
 
     public Page<Ticket> findAllByUserId(final Long userId, final Pageable pageable) {
+
         return ticketRepository.findAllByUserId(userId, pageable)
             .map(this.ticketPersistenceMapper::toDomain);
     }
 
     public Ticket findById(final Long ticketId) {
+
         final TicketEntity ticketEntity = this.ticketRepository.findById(ticketId)
             .orElseThrow(() -> ApplicationException.from(TicketErrorCode.TICKET_NOT_FOUND));
         return this.ticketPersistenceMapper.toDomain(ticketEntity);
+    }
+
+
+    public Long getTicketCountByCategoryAndDateRange(final Long categoryId, final LocalDate start, final LocalDate end) {
+
+        return ticketRepository.getTicketCountByCategoryAndDateRange(categoryId, start, end);
     }
 }
