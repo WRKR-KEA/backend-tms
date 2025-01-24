b8c5ccbb1db20e0dbc511528d1e29a5299b74f60
strongmhk <kim6562166086@gmail.com>
2025-01-24 14:02:25 +0900

    WRKR-85 ✨ Feature: Query DSL 리포지토리 생성 및 상속
    <body>
    복잡한 쿼리는 MemberQueryDslRepository에 정의해서 사용합니다
    
    <footer>
    #52

---
+++ src/main/java/com/wrkr/tickety/domains/member/persistence/repository/MemberQueryDslRepository.java
@@ -0,0 +1,11 @@
+package com.wrkr.tickety.domains.member.persistence.repository;
+
+import com.wrkr.tickety.domains.member.domain.constant.Role;
+import com.wrkr.tickety.domains.member.persistence.entity.MemberEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface MemberQueryDslRepository {
+    Page<MemberEntity> pagingByRole(Pageable pageable, Role role);
+}
--- src/main/java/com/wrkr/tickety/domains/member/persistence/repository/MemberRepository.java
+++ src/main/java/com/wrkr/tickety/domains/member/persistence/repository/MemberRepository.java
@@ -3,7 +3,7 @@ package com.wrkr.tickety.domains.member.persistence.repository;
 import com.wrkr.tickety.domains.member.persistence.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+public interface MemberRepository extends JpaRepository<MemberEntity, Long>, MemberQueryDslRepository {
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
 }
