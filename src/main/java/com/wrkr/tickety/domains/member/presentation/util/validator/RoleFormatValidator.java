339a96eb16a3fcace8fe8734409f9f56cd79f8f9
strongmhk <kim6562166086@gmail.com>
2025-01-26 00:55:50 +0900

    WRKR-85 ♻️ Refactor: Role 유효성 검증 값 변경
    <footer>
    #52

--- src/main/java/com/wrkr/tickety/domains/member/presentation/util/validator/RoleFormatValidator.java
+++ src/main/java/com/wrkr/tickety/domains/member/presentation/util/validator/RoleFormatValidator.java
@@ -18,7 +18,7 @@ public class RoleFormatValidator implements ConstraintValidator<RoleFormat, Role
         boolean isValid = false;
 
         for (Role r : Role.values()) {
-            if (r.name().equals(role)) {
+            if (r.equals(role)) {
                 isValid = true;
                 break;
             }
