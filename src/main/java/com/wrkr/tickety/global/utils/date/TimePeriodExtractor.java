5e963653131a6dc44d03bf0715188bfb16e9a6e7
strongmhk <kim6562166086@gmail.com>
2025-01-28 03:25:53 +0900

    WRKR-96 ✨ Feature: 통계 쿼리 기간 추출 클래스 생성
    <footer>
    #67

---
+++ src/main/java/com/wrkr/tickety/global/utils/date/TimePeriodExtractor.java
@@ -0,0 +1,54 @@
+package com.wrkr.tickety.global.utils.date;
+
+import com.wrkr.tickety.domains.ticket.domain.constant.StatisticsType;
+import com.wrkr.tickety.global.exception.ApplicationException;
+import com.wrkr.tickety.global.response.code.CommonErrorCode;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+public class TimePeriodExtractor {
+
+    public static TimePeriod extractTimePeriod(LocalDateTime dateTime, StatisticsType statisticsType) {
+        LocalDateTime startDateTime;
+        LocalDateTime endDateTime;
+
+        switch (statisticsType) {
+            case YEARLY -> {
+                startDateTime = dateTime.withDayOfYear(1).toLocalDate().atStartOfDay();
+                endDateTime = dateTime.plusYears(1).withDayOfYear(1).toLocalDate().atStartOfDay();
+            }
+            case MONTHLY -> {
+                startDateTime = dateTime.withDayOfMonth(1).toLocalDate().atStartOfDay();
+                endDateTime = dateTime.plusMonths(1).withDayOfMonth(1).toLocalDate().atStartOfDay();
+            }
+            case DAILY -> {
+                startDateTime = dateTime.toLocalDate().atStartOfDay();
+                endDateTime = dateTime.toLocalDate().plusDays(1).atStartOfDay();
+            }
+            case TOTAL -> {
+                // datetime 최솟값: 1000-01-01 00:00:00.000000
+                startDateTime = LocalDateTime.of(1000, 1, 1, 0, 0); // datetime 최솟값
+                endDateTime = LocalDateTime.now();
+            }
+            default -> {
+                throw new ApplicationException(CommonErrorCode.METHOD_ARGUMENT_NOT_VALID);
+            }
+        }
+
+        return new TimePeriod(startDateTime, endDateTime);
+    }
+
+    @Getter
+    @Builder
+    public static class TimePeriod {
+
+        private final LocalDateTime startDateTime;
+        private final LocalDateTime endDateTime;
+
+        public TimePeriod(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+            this.startDateTime = startDateTime;
+            this.endDateTime = endDateTime;
+        }
+    }
+}
