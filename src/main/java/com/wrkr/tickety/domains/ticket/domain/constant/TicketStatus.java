f494cd297d622594f422d524122fa6932549615d
strongmhk <kim6562166086@gmail.com>
2025-01-28 03:33:15 +0900

    WRKR-96 ✨ Feature: TicketStatus 역직렬화 메서드 구현
    <footer>
    #67

--- src/main/java/com/wrkr/tickety/domains/ticket/domain/constant/TicketStatus.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/domain/constant/TicketStatus.java
@@ -1,5 +1,7 @@
 package com.wrkr.tickety.domains.ticket.domain.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -11,8 +13,15 @@ public enum TicketStatus {
     IN_PROGRESS("진행"),
     COMPLETE("완료"),
     CANCEL("취소"),
-    REJECT("반려")
-    ;
+    REJECT("반려");
 
     private final String description;
+
+    @JsonCreator
+    public static TicketStatus from(String status) {
+        return Arrays.stream(TicketStatus.values())
+            .filter(ticketStatus -> ticketStatus.name().equalsIgnoreCase(status))
+            .findFirst()
+            .orElse(null);
+    }
 }
