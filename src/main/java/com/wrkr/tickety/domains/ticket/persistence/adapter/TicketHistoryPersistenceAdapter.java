77adddfc005413186b2c3d961c0f1bb200b98ab0
strongmhk <kim6562166086@gmail.com>
2025-01-28 03:28:32 +0900

    WRKR-96 ✨ Feature: PersistenceAdapter Layer
    <footer>
    #67

--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/adapter/TicketHistoryPersistenceAdapter.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/adapter/TicketHistoryPersistenceAdapter.java
@@ -1,15 +1,18 @@
 package com.wrkr.tickety.domains.ticket.persistence.adapter;
 
+import com.wrkr.tickety.domains.ticket.application.dto.response.statistics.StatisticsByTicketStatusResponse.TicketCount;
 import com.wrkr.tickety.domains.ticket.domain.constant.ModifiedType;
+import com.wrkr.tickety.domains.ticket.domain.constant.StatisticsType;
+import com.wrkr.tickety.domains.ticket.domain.constant.TicketStatus;
 import com.wrkr.tickety.domains.ticket.domain.model.TicketHistory;
 import com.wrkr.tickety.domains.ticket.persistence.entity.TicketHistoryEntity;
 import com.wrkr.tickety.domains.ticket.persistence.mapper.TicketHistoryPersistenceMapper;
 import com.wrkr.tickety.domains.ticket.persistence.repository.TicketHistoryRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -26,9 +29,23 @@ public class TicketHistoryPersistenceAdapter {
 
     public Optional<TicketHistory> findFirstByTicketIdAndModifiedOrderByCreatedAtAsc(Long ticketId, ModifiedType modifiedType) {
         TicketHistoryEntity ticketHistoryEntity = ticketHistoryRepository.findFirstByTicket_TicketIdAndModifiedOrderByCreatedAtAsc(
-                ticketId,
-                modifiedType
+            ticketId,
+            modifiedType
         );
         return Optional.ofNullable(ticketHistoryPersistenceMapper.toDomain(ticketHistoryEntity));
+    }
+
+    public List<TicketCount> countByTicketStatusAndPeriod(
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        StatisticsType statisticsType,
+        TicketStatus ticketStatus
+    ) {
+        return this.ticketHistoryRepository.countByTicketStatusDuringPeriod(
+            startDate,
+            endDate,
+            statisticsType,
+            ticketStatus
+        );
     }
 }
