415b4451e0f3e6d8238e88179ac6d3ba97ed7e68
strongmhk <kim6562166086@gmail.com>
2025-01-28 03:30:11 +0900

    WRKR-96 ✨ Feature: Service Layer
    <footer>
    #67

--- src/main/java/com/wrkr/tickety/domains/ticket/domain/service/tickethistory/TicketHistoryGetService.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/domain/service/tickethistory/TicketHistoryGetService.java
@@ -1,14 +1,16 @@
 package com.wrkr.tickety.domains.ticket.domain.service.tickethistory;
 
+import com.wrkr.tickety.domains.ticket.application.dto.response.statistics.StatisticsByTicketStatusResponse.TicketCount;
 import com.wrkr.tickety.domains.ticket.domain.constant.ModifiedType;
+import com.wrkr.tickety.domains.ticket.domain.constant.StatisticsType;
+import com.wrkr.tickety.domains.ticket.domain.constant.TicketStatus;
 import com.wrkr.tickety.domains.ticket.domain.model.TicketHistory;
 import com.wrkr.tickety.domains.ticket.persistence.adapter.TicketHistoryPersistenceAdapter;
-import com.wrkr.tickety.domains.ticket.persistence.repository.TicketHistoryRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -17,11 +19,24 @@ public class TicketHistoryGetService {
 
     private final TicketHistoryPersistenceAdapter ticketHistoryPersistenceAdapter;
 
-
     public LocalDateTime getFirstManagerChangeDate(Long ticketId) {
         return ticketHistoryPersistenceAdapter.findFirstByTicketIdAndModifiedOrderByCreatedAtAsc(
-                ticketId,
-                ModifiedType.MANAGER
+            ticketId,
+            ModifiedType.MANAGER
         ).map(TicketHistory::getCreatedAt).orElse(null);
+    }
+
+    public List<TicketCount> getTicketCountStatistics(
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        StatisticsType statisticsType,
+        TicketStatus ticketStatus
+    ) {
+        return ticketHistoryPersistenceAdapter.countByTicketStatusAndPeriod(
+            startDate,
+            endDate,
+            statisticsType,
+            ticketStatus
+        );
     }
 }
