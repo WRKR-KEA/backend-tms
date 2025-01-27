a4856268ab8bbf8a553ee92ba3f0508b99c1419c
strongmhk <kim6562166086@gmail.com>
2025-01-28 03:27:39 +0900

    WRKR-96 ✨ Feature: TicketHistory QueryDsl 전용 리포지토리 생성 및 상속
    <footer>
    #67

---
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketHistoryQueryDslRepositoryImpl.java
@@ -0,0 +1,76 @@
+package com.wrkr.tickety.domains.ticket.persistence.repository;
+
+import static com.wrkr.tickety.domains.ticket.persistence.entity.QTicketHistoryEntity.ticketHistoryEntity;
+
+import com.querydsl.core.types.ConstantImpl;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.StringTemplate;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.wrkr.tickety.domains.ticket.application.dto.response.statistics.QStatisticsByTicketStatusResponse_TicketCount;
+import com.wrkr.tickety.domains.ticket.application.dto.response.statistics.StatisticsByTicketStatusResponse.TicketCount;
+import com.wrkr.tickety.domains.ticket.domain.constant.StatisticsType;
+import com.wrkr.tickety.domains.ticket.domain.constant.TicketStatus;
+import com.wrkr.tickety.domains.ticket.persistence.entity.QTicketHistoryEntity;
+import com.wrkr.tickety.global.exception.ApplicationException;
+import com.wrkr.tickety.global.response.code.CommonErrorCode;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class TicketHistoryQueryDslRepositoryImpl implements TicketHistoryQueryDslRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public List<TicketCount> countByTicketStatusDuringPeriod(
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        StatisticsType statisticsType,
+        TicketStatus ticketStatus
+    ) {
+        QTicketHistoryEntity th = ticketHistoryEntity;
+
+        StringTemplate dateFormat = getDateFormatTemplate(statisticsType, th);
+
+        List<TicketCount> countList = jpaQueryFactory
+            .select(
+                new QStatisticsByTicketStatusResponse_TicketCount(
+                    dateFormat,
+                    th.ticketHistoryId.count()
+                )
+            )
+            .from(th)
+            .where(
+                betweenPeriod(startDate, endDate),
+                statusEq(ticketStatus)
+            )
+            .groupBy(dateFormat)
+            .orderBy(dateFormat.asc())
+            .fetch();
+
+        return countList;
+    }
+
+    private StringTemplate getDateFormatTemplate(StatisticsType statisticsType, QTicketHistoryEntity th) {
+        return switch (statisticsType) {
+            case TOTAL -> Expressions.stringTemplate("DATE_FORMAT({0}, {1})", th.createdAt, ConstantImpl.create("%Y"));
+            case YEARLY -> Expressions.stringTemplate("DATE_FORMAT({0}, {1})", th.createdAt, ConstantImpl.create("%Y-%m"));
+            case MONTHLY -> Expressions.stringTemplate("DATE_FORMAT({0}, {1})", th.createdAt, ConstantImpl.create("%Y-%m-%d"));
+            case DAILY -> Expressions.stringTemplate("DATE_FORMAT({0}, {1})", th.createdAt, ConstantImpl.create("%Y-%m-%d %H"));
+            default -> throw new ApplicationException(CommonErrorCode.METHOD_ARGUMENT_NOT_VALID);
+        };
+    }
+
+    private BooleanExpression statusEq(TicketStatus status) {
+        return status == null ? null : ticketHistoryEntity.status.eq(status);
+    }
+
+    private BooleanExpression betweenPeriod(LocalDateTime startDate, LocalDateTime endDate) {
+        // datetime의 최솟값: 1000-01-01 00:00:00.000000
+        return startDate == null || endDate == null || startDate.getYear() == 1000 ? null : ticketHistoryEntity.createdAt.between(startDate, endDate);
+    }
+}
+
--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketHistoryRepository.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketHistoryRepository.java
@@ -1,16 +1,13 @@
 package com.wrkr.tickety.domains.ticket.persistence.repository;
 
 import com.wrkr.tickety.domains.ticket.domain.constant.ModifiedType;
-import com.wrkr.tickety.domains.ticket.domain.model.TicketHistory;
 import com.wrkr.tickety.domains.ticket.persistence.entity.TicketHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface TicketHistoryRepository extends JpaRepository<TicketHistoryEntity, Long> {
+public interface TicketHistoryRepository extends JpaRepository<TicketHistoryEntity, Long>, TicketHistoryQueryDslRepository {
 
     TicketHistoryEntity findFirstByTicket_TicketIdAndModifiedOrderByCreatedAtAsc(
-            Long ticketId,
-            ModifiedType modifiedType
+        Long ticketId,
+        ModifiedType modifiedType
     );
 }
