9386fd8d53f13813772efb027027212399459830
hurrayPersimmon <rahel337@naver.com>
2025-01-28 15:35:46 +0900

    WRKR-94 :recycle: Refactor: Category ErrorCode 추가

--- src/main/java/com/wrkr/tickety/global/annotation/swagger/CustomErrorCodes.java
+++ src/main/java/com/wrkr/tickety/global/annotation/swagger/CustomErrorCodes.java
@@ -3,11 +3,9 @@ package com.wrkr.tickety.global.annotation.swagger;
 import com.wrkr.tickety.domains.ticket.exception.CommentErrorCode;
 import com.wrkr.tickety.domains.member.exception.MemberErrorCode;
 import com.wrkr.tickety.domains.ticket.exception.CategoryErrorCode;
-import com.wrkr.tickety.domains.ticket.exception.CommentErrorCode;
 import com.wrkr.tickety.global.response.code.CommonErrorCode;
 import com.wrkr.tickety.domains.ticket.exception.GuideErrorCode;
 import com.wrkr.tickety.domains.ticket.exception.TicketErrorCode;
-import com.wrkr.tickety.global.response.code.CommonErrorCode;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -24,6 +22,8 @@ public @interface CustomErrorCodes {
     TicketErrorCode[] ticketErrorCodes() default {};
 
     CommentErrorCode[] commentErrorCodes() default {};
+
+    CategoryErrorCode[] categoryErrorCodes() default {};
 
     GuideErrorCode[] value() default {};
 }
