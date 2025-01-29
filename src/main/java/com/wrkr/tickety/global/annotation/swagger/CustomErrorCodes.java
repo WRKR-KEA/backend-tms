da525ae876ff18a65e453ec2b347faa7d70aa7ee
strongmhk <kim6562166086@gmail.com>
2025-01-29 18:52:53 +0900

    WRKR-96 ✨ Feature: CustomErrorCodes에 새로운 값 추가
    <footer>
    #67

--- src/main/java/com/wrkr/tickety/global/annotation/swagger/CustomErrorCodes.java
+++ src/main/java/com/wrkr/tickety/global/annotation/swagger/CustomErrorCodes.java
@@ -1,11 +1,9 @@
 package com.wrkr.tickety.global.annotation.swagger;
 
-import com.wrkr.tickety.domains.ticket.exception.CommentErrorCode;
 import com.wrkr.tickety.domains.member.exception.MemberErrorCode;
-import com.wrkr.tickety.domains.ticket.exception.CategoryErrorCode;
 import com.wrkr.tickety.domains.ticket.exception.CommentErrorCode;
-import com.wrkr.tickety.global.response.code.CommonErrorCode;
 import com.wrkr.tickety.domains.ticket.exception.GuideErrorCode;
+import com.wrkr.tickety.domains.ticket.exception.StatisticsErrorCode;
 import com.wrkr.tickety.domains.ticket.exception.TicketErrorCode;
 import com.wrkr.tickety.global.response.code.CommonErrorCode;
 import java.lang.annotation.ElementType;
@@ -20,10 +18,12 @@ public @interface CustomErrorCodes {
     CommonErrorCode[] commonErrorCodes() default {};
 
     MemberErrorCode[] memberErrorCodes() default {};
-  
+
     TicketErrorCode[] ticketErrorCodes() default {};
 
     CommentErrorCode[] commentErrorCodes() default {};
 
     GuideErrorCode[] value() default {};
+
+    StatisticsErrorCode[] statisticsErrorCodes() default {};
 }
