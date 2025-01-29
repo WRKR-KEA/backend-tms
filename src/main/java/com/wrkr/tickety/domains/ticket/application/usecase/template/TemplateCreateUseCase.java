0777ce283de25d2120ec0b241f9ff6f810fe8f66
hurrayPersimmon <rahel337@naver.com>
2025-01-29 14:12:54 +0900

    WRKR-106 :recycle: Refactor: save 메소드 공통 사용을 위한 메소드 수정

--- src/main/java/com/wrkr/tickety/domains/ticket/application/usecase/template/TemplateCreateUseCase.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/usecase/template/TemplateCreateUseCase.java
@@ -7,7 +7,9 @@ import com.wrkr.tickety.domains.ticket.domain.model.Category;
 import com.wrkr.tickety.domains.ticket.domain.model.Template;
 import com.wrkr.tickety.domains.ticket.domain.service.category.CategoryGetService;
 import com.wrkr.tickety.domains.ticket.domain.service.template.TemplateCreateService;
+import com.wrkr.tickety.domains.ticket.domain.service.template.TemplateGetService;
 import com.wrkr.tickety.domains.ticket.exception.CategoryErrorCode;
+import com.wrkr.tickety.domains.ticket.exception.TemplateErrorCode;
 import com.wrkr.tickety.global.annotation.architecture.UseCase;
 import com.wrkr.tickety.global.exception.ApplicationException;
 import com.wrkr.tickety.global.utils.PkCrypto;
@@ -19,11 +21,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class TemplateCreateUseCase {
 
+    private final TemplateGetService templateGetService;
     private final TemplateCreateService templateCreateService;
     private final CategoryGetService categoryGetService;
 
     public TemplatePKResponse createTemplate(AdminTemplateCreateRequest request) {
-        Category category = categoryGetService.getCategory(PkCrypto.decrypt(request.categoryId())).orElseThrow(() -> new ApplicationException(CategoryErrorCode.CATEGORY_NOT_EXIST));
+        Category category = categoryGetService.getCategory(PkCrypto.decrypt(request.categoryId())).orElseThrow(() -> new ApplicationException(CategoryErrorCode.CATEGORY_NOT_EXISTS));
+        if(category.getParent() != null){
+            throw new ApplicationException(TemplateErrorCode.TEMPLATE_CANNOT_BE_CREATED_FOR_SUBCATEGORY);
+        }
+        if(templateGetService.existsByCategoryId(category.getCategoryId())){
+            throw new ApplicationException(TemplateErrorCode.TEMPLATE_ALREADY_EXISTS);
+        }
+
         Template requestTemplate = TemplateMapper.mapToTemplateDomain(request, category);
         Template createdTemplate = templateCreateService.save(requestTemplate);
         return TemplateMapper.mapToTemplatePKResponse(createdTemplate);
