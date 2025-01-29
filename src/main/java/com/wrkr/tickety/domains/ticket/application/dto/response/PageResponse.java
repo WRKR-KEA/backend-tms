3a2cda552e93c5207e6d8da3e61ba4968d1172d6
Sujin Kang <1214sujin@naver.com>
2025-01-29 17:12:39 +0900

    WRKR-101 :memo: Docs: 페이징 응답 클래스 @Schema 작성
    <footer> #77

--- src/main/java/com/wrkr/tickety/domains/ticket/application/dto/response/PageResponse.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/dto/response/PageResponse.java
@@ -1,15 +1,25 @@
 package com.wrkr.tickety.domains.ticket.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.function.Function;
 import org.springframework.data.domain.Page;
 
 public record PageResponse<T>(
 
+    @Schema(description = "페이지 내용")
     List<T> elements,
+
+    @Schema(description = "현재 페이지 번호", example = "1")
     Integer currentPage,
+
+    @Schema(description = "총 페이지", example = "3")
     Integer totalPages,
+
+    @Schema(description = "총계", example = "50")
     Long totalElements,
+
+    @Schema(description = "페이지 사이즈", example = "20")
     Integer size
 ) {
 
