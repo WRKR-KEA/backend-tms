e2803ba848f21f410423e15359196a32b0ef67bb
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-01-31 17:18:56 +0900

    WRKR-110 ♻️ Refactor: 사용하지 않는 임포트문 제거

--- src/main/java/com/wrkr/tickety/domains/ticket/domain/service/ticket/TicketGetService.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/domain/service/ticket/TicketGetService.java
@@ -7,7 +7,6 @@ import com.wrkr.tickety.domains.ticket.persistence.adapter.TicketPersistenceAdap
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketRepository.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/repository/TicketRepository.java
@@ -1,11 +1,9 @@
 package com.wrkr.tickety.domains.ticket.persistence.repository;
 
-import com.wrkr.tickety.domains.ticket.domain.constant.TicketStatus;
 import com.wrkr.tickety.domains.ticket.persistence.entity.TicketEntity;
 import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
