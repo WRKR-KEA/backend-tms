aae415ca5a27f6e7951a4a7b3bf7186ed5378a65
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-01-31 17:13:53 +0900

    WRKR-110 ✨ Feature: 서비스 정렬 타입 생성

--- src/main/java/com/wrkr/tickety/domains/ticket/domain/service/ticket/TicketGetService.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/domain/service/ticket/TicketGetService.java
@@ -1,11 +1,13 @@
 package com.wrkr.tickety.domains.ticket.domain.service.ticket;
 
+import com.wrkr.tickety.domains.ticket.domain.constant.SortType;
 import com.wrkr.tickety.domains.ticket.domain.constant.TicketStatus;
 import com.wrkr.tickety.domains.ticket.domain.model.Ticket;
 import com.wrkr.tickety.domains.ticket.persistence.adapter.TicketPersistenceAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +26,7 @@ public class TicketGetService {
         return ticketPersistenceAdapter.findById(ticketId);
     }
 
-    public Page<Ticket> getTicketsByManagerFilter(Long managerId, Pageable pageable, TicketStatus status, String query) {
-        return ticketPersistenceAdapter.findAllByManagerFilter(managerId, pageable, status, query);
+    public Page<Ticket> getTicketsByManagerFilter(Long managerId, Pageable pageable, TicketStatus status, String query, SortType sortType) {
+        return ticketPersistenceAdapter.findAllByManagerFilter(managerId, pageable, status, query, sortType);
     }
 }
