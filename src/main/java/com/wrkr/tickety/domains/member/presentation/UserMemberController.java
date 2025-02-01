8b8b77b61f39a2e16801b67f7917554c38bef17b
strongmhk <kim6562166086@gmail.com>
2025-02-02 00:35:24 +0900

    WRKR-116 📝 Docs: 스웨거 애노테이션 추가
    <footer>
    #105

--- src/main/java/com/wrkr/tickety/domains/member/presentation/UserMemberController.java
+++ src/main/java/com/wrkr/tickety/domains/member/presentation/UserMemberController.java
@@ -1,10 +1,14 @@
 package com.wrkr.tickety.domains.member.presentation;
 
+import static com.wrkr.tickety.domains.member.exception.MemberErrorCode.INVALID_PASSWORD_FORMAT;
+
 import com.wrkr.tickety.domains.member.application.dto.request.PasswordUpdateRequest;
 import com.wrkr.tickety.domains.member.application.dto.response.MemberPkResponse;
 import com.wrkr.tickety.domains.member.application.usecase.PasswordUpdateUseCase;
 import com.wrkr.tickety.domains.member.domain.model.Member;
+import com.wrkr.tickety.global.annotation.swagger.CustomErrorCodes;
 import com.wrkr.tickety.global.response.ApplicationResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +26,8 @@ public class UserMemberController {
 
     private final PasswordUpdateUseCase passwordUpdateUseCase;
 
+    @Operation(summary = "비밀번호 재설정", description = "사용자 또는 담당자가 비밀번호 재설정을 진행합니다.")
+    @CustomErrorCodes(memberErrorCodes = {INVALID_PASSWORD_FORMAT})
     @PatchMapping
     public ApplicationResponse<MemberPkResponse> updatePassword(
         @AuthenticationPrincipal Member member,
