1c36fc16fb19524bbc6d577c04326d2c3648a6cf
hurrayPersimmon <rahel337@naver.com>
2025-02-04 02:02:31 +0900

    WRKR-106 :recycle: Refactor: TemplateErrorCode INVALID TEMPLATE 삭제

--- src/main/java/com/wrkr/tickety/domains/ticket/exception/TemplateErrorCode.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/exception/TemplateErrorCode.java
@@ -9,9 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum TemplateErrorCode implements BaseErrorCode {
 
-    TEMPLATE_CANNOT_DO_FOR_SUBCATEGORY_OR_DELETED(HttpStatus.BAD_REQUEST, "TEMPLATE_001", "2차 카테고리 혹은 삭제된 카테고리입니다."),
     TEMPLATE_NOT_EXISTS(HttpStatus.NOT_FOUND, "TEMPLATE_401", "템플릿이 존재하지 않습니다."),
-    TEMPLATE_ALREADY_EXISTS(HttpStatus.CONFLICT, "TEMPLATE_901", "템플릿이 이미 존재합니다." );
+    TEMPLATE_ALREADY_EXISTS(HttpStatus.CONFLICT, "TEMPLATE_901", "템플릿이 이미 존재합니다.");
 
     private final HttpStatus httpStatus;
     private final String customCode;
