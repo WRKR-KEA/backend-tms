f3cb49e1f2686a431069524c2ee25a36949196ff
hurrayPersimmon <rahel337@naver.com>
2025-02-03 14:44:17 +0900

    WRKR-94 :recycle: Refactor: 티켓 카테고리 삭제 API 리팩터링

--- src/main/java/com/wrkr/tickety/domains/ticket/application/usecase/category/CategoryDeleteUseCase.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/application/usecase/category/CategoryDeleteUseCase.java
@@ -1,16 +1,14 @@
 package com.wrkr.tickety.domains.ticket.application.usecase.category;
 
-import com.wrkr.tickety.domains.ticket.application.dto.response.PkResponse;
+import com.wrkr.tickety.domains.ticket.application.dto.response.CategoryPkResponse.CategoryPK;
 import com.wrkr.tickety.domains.ticket.application.mapper.CategoryMapper;
 import com.wrkr.tickety.domains.ticket.domain.model.Category;
 import com.wrkr.tickety.domains.ticket.domain.service.category.CategoryDeleteService;
 import com.wrkr.tickety.domains.ticket.domain.service.category.CategoryGetService;
-import com.wrkr.tickety.domains.ticket.exception.CategoryErrorCode;
 import com.wrkr.tickety.global.annotation.architecture.UseCase;
-import com.wrkr.tickety.global.exception.ApplicationException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
 
 
 @UseCase
@@ -21,19 +19,16 @@ public class CategoryDeleteUseCase {
     private final CategoryGetService categoryGetService;
     private final CategoryDeleteService categoryDeleteService;
 
-    public PkResponse softDeleteCategory(Long categoryId) {
-        Category reqeustCategory = categoryGetService.getCategory(categoryId)
-            .orElseThrow(() -> ApplicationException.from(CategoryErrorCode.CATEGORY_NOT_EXIST));
-        if(reqeustCategory.getIsDeleted()) throw ApplicationException.from(CategoryErrorCode.CATEGORY_ALREADY_DELETED);
-
-        List<Category> requestChildren = categoryGetService.getChildren(categoryId);
-
-        List<Category> children = requestChildren.stream()
-                .map(Category::softDeleteCategory)
-                .toList();
-        Category category = Category.softDeleteCategory(reqeustCategory);
-        Category deletedCategory = categoryDeleteService.softDeleteCategory(category, children);
+    public CategoryPK softDeleteCategory(Long categoryId) {
+        Category findCategory = categoryGetService.getCategory(categoryId);
+        Category deletedCategory = categoryDeleteService.softDeleteCategory(findCategory);
+        softDeleteChildCategories(categoryId);
         return CategoryMapper.mapToPkResponse(deletedCategory);
+    }
+
+    private void softDeleteChildCategories(Long categoryId) {
+        List<Category> children = categoryGetService.getChildren(categoryId);
+        categoryDeleteService.softDeleteCategories(children);
     }
 }
 
