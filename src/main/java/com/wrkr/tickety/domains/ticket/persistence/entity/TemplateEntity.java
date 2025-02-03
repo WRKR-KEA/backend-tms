87cce935e9032cb90deac63fc7634ff2cbf1be8c
hurrayPersimmon <rahel337@naver.com>
2025-02-04 01:59:57 +0900

    WRKR-106 :recycle: Refactor: Category Builder 추가

--- src/main/java/com/wrkr/tickety/domains/ticket/persistence/entity/TemplateEntity.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/persistence/entity/TemplateEntity.java
@@ -1,8 +1,17 @@
 package com.wrkr.tickety.domains.ticket.persistence.entity;
 
 import com.wrkr.tickety.global.entity.BaseTimeEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -26,6 +35,17 @@ public class TemplateEntity extends BaseTimeEntity {
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
+
+    @Builder
+    public TemplateEntity(
+        Long templateId,
+        CategoryEntity category,
+        String content
+    ) {
+        this.templateId = templateId;
+        this.category = category;
+        this.content = content;
+    }
 
 }
 
