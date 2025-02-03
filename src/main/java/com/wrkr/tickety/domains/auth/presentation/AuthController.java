ab9475c3e9f9c38fbf588be26e5951dea16f8c1d
jiwon <kiwijomn@gmail.com>
2025-02-03 15:46:07 +0900

    WRKR-131 :bug: Fix: 로그인 및 토큰 재발급 API response 매퍼 수정
    <footer>
    #124

--- src/main/java/com/wrkr/tickety/domains/auth/presentation/AuthController.java
+++ src/main/java/com/wrkr/tickety/domains/auth/presentation/AuthController.java
@@ -7,7 +7,7 @@ import static com.wrkr.tickety.domains.auth.exception.AuthErrorCode.TOKEN_NOT_FO
 import static com.wrkr.tickety.domains.member.exception.MemberErrorCode.MEMBER_NOT_FOUND;
 
 import com.wrkr.tickety.domains.auth.application.dto.request.LoginRequest;
-import com.wrkr.tickety.domains.auth.application.dto.response.TokenResponse;
+import com.wrkr.tickety.domains.auth.application.dto.response.AuthTokenResponse;
 import com.wrkr.tickety.domains.auth.application.usecase.AuthTokenUseCase;
 import com.wrkr.tickety.domains.auth.application.usecase.LoginUseCase;
 import com.wrkr.tickety.domains.member.domain.model.Member;
@@ -35,17 +35,17 @@ public class AuthController {
     @PostMapping("/login")
     @CustomErrorCodes(memberErrorCodes = {MEMBER_NOT_FOUND}, authErrorCodes = {INVALID_CREDENTIALS, ACCOUNT_LOCKED})
     @Operation(summary = "로그인", description = "회원이 닉네임과 비밀번호로 로그인을 합니다.")
-    public ApplicationResponse<TokenResponse> login(@RequestBody LoginRequest request) {
+    public ApplicationResponse<AuthTokenResponse> login(@RequestBody LoginRequest request) {
         Member member = loginUseCase.login(request);
-        TokenResponse response = authTokenUseCase.generateToken(member);
+        AuthTokenResponse response = authTokenUseCase.generateToken(member);
         return ApplicationResponse.onSuccess(response);
     }
 
     @PostMapping("/refresh")
     @CustomErrorCodes(memberErrorCodes = {MEMBER_NOT_FOUND}, authErrorCodes = {INVALID_TOKEN, TOKEN_NOT_FOUND})
     @Operation(summary = "토큰 재발급", description = "리프레시 토큰으로 토큰을 재발급 받습니다.")
-    public ApplicationResponse<TokenResponse> refresh(@AuthenticationPrincipal Member member, HttpServletRequest request) {
-        TokenResponse response = authTokenUseCase.reissueToken(member, request);
+    public ApplicationResponse<AuthTokenResponse> refresh(@AuthenticationPrincipal Member member, HttpServletRequest request) {
+        AuthTokenResponse response = authTokenUseCase.reissueToken(member, request);
         return ApplicationResponse.onSuccess(response);
     }
 }
