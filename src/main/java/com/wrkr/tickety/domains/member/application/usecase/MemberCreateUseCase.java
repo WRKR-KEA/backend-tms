d1cf5ed02f3597de63733fdf916c92e210c282d6
strongmhk <kim6562166086@gmail.com>
2025-02-04 10:26:59 +0900

    WRKR-118 🚚 Rename: 클래스 이동 및 이름 변경
    <body>
    EmailCreateUseCase-> EmailUtil
    
    <footer>
    #109

--- src/main/java/com/wrkr/tickety/domains/member/application/usecase/MemberCreateUseCase.java
+++ src/main/java/com/wrkr/tickety/domains/member/application/usecase/MemberCreateUseCase.java
@@ -6,12 +6,13 @@ import com.wrkr.tickety.domains.member.application.dto.request.MemberCreateReque
 import com.wrkr.tickety.domains.member.application.dto.response.MemberPkResponse;
 import com.wrkr.tickety.domains.member.application.mapper.EmailMapper;
 import com.wrkr.tickety.domains.member.application.mapper.MemberMapper;
-import com.wrkr.tickety.domains.member.domain.constant.EmailConstants;
 import com.wrkr.tickety.domains.member.domain.model.Member;
 import com.wrkr.tickety.domains.member.domain.service.MemberSaveService;
 import com.wrkr.tickety.global.annotation.architecture.UseCase;
 import com.wrkr.tickety.global.utils.PkCrypto;
 import com.wrkr.tickety.global.utils.UUIDGenerator;
+import com.wrkr.tickety.infrastructure.email.EmailConstants;
+import com.wrkr.tickety.infrastructure.email.EmailUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberCreateUseCase {
 
     private final MemberSaveService memberSaveService;
-    private final EmailCreateUseCase emailCreateUseCase; // TODO: UseCase간 서로 참조하도록 해도 되는지 고민 필요
+    private final EmailUtil emailUtil;
 
     public MemberPkResponse createMember(MemberCreateRequest memberCreateRequest) {
         String tempPassword = UUIDGenerator.generateUUID().substring(0, 12);
@@ -30,7 +31,7 @@ public class MemberCreateUseCase {
         Member createdMember = memberSaveService.save(MemberMapper.toMember(memberCreateRequest, encryptedPassword));
 
         EmailCreateRequest emailCreateRequest = EmailMapper.toEmailCreateRequest(createdMember.getEmail(), EmailConstants.TEMP_PASSWORD_SUBJECT, null);
-        emailCreateUseCase.sendMail(emailCreateRequest, tempPassword, EmailConstants.TYPE_PASSWORD);
+        emailUtil.sendMail(emailCreateRequest, tempPassword, EmailConstants.TYPE_PASSWORD);
 
         return MemberMapper.toMemberPkResponse(PkCrypto.encrypt(createdMember.getMemberId()));
     }
