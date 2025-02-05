869a8a46def37c42ae5e940a595ff4c71fa04926
strongmhk <kim6562166086@gmail.com>
2025-02-05 23:49:22 +0900

    WRKR-141 ✨ Feature: 비밀번호 재발급 API 구현
    <footer>
    #144

--- src/main/java/com/wrkr/tickety/domains/member/presentation/UserMemberController.java
+++ src/main/java/com/wrkr/tickety/domains/member/presentation/UserMemberController.java
@@ -2,8 +2,10 @@ package com.wrkr.tickety.domains.member.presentation;
 
 import static com.wrkr.tickety.domains.member.exception.MemberErrorCode.INVALID_PASSWORD_FORMAT;
 
+import com.wrkr.tickety.domains.member.application.dto.request.PasswordReissueRequest;
 import com.wrkr.tickety.domains.member.application.dto.request.PasswordUpdateRequest;
 import com.wrkr.tickety.domains.member.application.dto.response.MemberPkResponse;
+import com.wrkr.tickety.domains.member.application.usecase.PasswordReissueUseCase;
 import com.wrkr.tickety.domains.member.application.usecase.PasswordUpdateUseCase;
 import com.wrkr.tickety.domains.member.domain.model.Member;
 import com.wrkr.tickety.global.annotation.swagger.CustomErrorCodes;
@@ -25,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserMemberController {
 
     private final PasswordUpdateUseCase passwordUpdateUseCase;
+    private final PasswordReissueUseCase passwordReissueUseCase;
+
 
     @Operation(summary = "비밀번호 재설정", description = "사용자 또는 담당자가 비밀번호 재설정을 진행합니다.")
     @CustomErrorCodes(memberErrorCodes = {INVALID_PASSWORD_FORMAT})
@@ -34,6 +38,17 @@ public class UserMemberController {
         @RequestBody @Valid PasswordUpdateRequest passwordUpdateRequest
     ) {
         MemberPkResponse memberPkResponse = passwordUpdateUseCase.updatePassword(member.getMemberId(), passwordUpdateRequest.password());
+        return ApplicationResponse.onSuccess(memberPkResponse);
+    }
+
+    @Operation(summary = "비밀번호 재발급", description = "사용자 또는 담당자는 이메일로 임시 비밀번호를 발급받을 수 있습니다.")
+    @CustomErrorCodes(memberErrorCodes = {INVALID_PASSWORD_FORMAT})
+    @PatchMapping("/password/reissue")
+    public ApplicationResponse<MemberPkResponse> regeneratePassword(
+        @AuthenticationPrincipal Member member,
+        @RequestBody @Valid PasswordReissueRequest passwordReissueRequest
+    ) {
+        MemberPkResponse memberPkResponse = passwordReissueUseCase.reissuePassword(passwordReissueRequest.nickname());
         return ApplicationResponse.onSuccess(memberPkResponse);
     }
 }
