887f9ceb73da8b81d101cfe386ea2af161087467
jiwon <kiwijomn@gmail.com>
2025-02-06 11:22:52 +0900

    :bug: Fix: cors origin pattern 와일드 카드 임시 수정

--- src/main/java/com/wrkr/tickety/global/config/security/SecurityConfig.java
+++ src/main/java/com/wrkr/tickety/global/config/security/SecurityConfig.java
@@ -84,10 +84,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:3000",
-            "http://localhost:8080"
-        ));
+        configuration.addAllowedOriginPattern("*");
 
         configuration.setAllowedHeaders(Arrays.asList("*"));
 
