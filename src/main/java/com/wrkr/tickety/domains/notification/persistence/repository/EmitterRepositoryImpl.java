95d9a9e943b537753bf4ab27bb81192d3e4b3c3f
jiwon <kiwijomn@gmail.com>
2025-02-07 12:08:30 +0900

    WRKR-169 :recycle: Refactor: EmitterRepository 구현체와 인터페이스 분리
    <footer>
    #165

--- src/main/java/com/wrkr/tickety/domains/notification/persistence/repository/EmitterRepositoryImpl.java
+++ src/main/java/com/wrkr/tickety/domains/notification/persistence/repository/EmitterRepositoryImpl.java
@@ -1,4 +1,4 @@
-package com.wrkr.tickety.domains.notification.presentation;
+package com.wrkr.tickety.domains.notification.persistence.repository;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -7,32 +7,37 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Repository
-public class EmitterRepository {
+public class EmitterRepositoryImpl implements EmitterRepository {
 
     private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
     private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
 
+    @Override
     public SseEmitter save(String emitterId, SseEmitter sseEmitter) {
         emitters.put(emitterId, sseEmitter);
         return sseEmitter;
     }
 
+    @Override
     public void saveEventCache(String emitterId, Object event) {
         eventCache.put(emitterId, event);
     }
 
+    @Override
     public Map<String, SseEmitter> findAllEmitterStartWithByMemberId(String memberId) {
         return emitters.entrySet().stream()
             .filter(entry -> entry.getKey().startsWith(memberId))
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
+    @Override
     public Map<String, Object> findAllEventCacheStartWithByMemberId(String memberId) {
         return eventCache.entrySet().stream()
             .filter(entry -> entry.getKey().startsWith(memberId))
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
+    @Override
     public void deleteById(String emitterId) {
         emitters.remove(emitterId);
     }
