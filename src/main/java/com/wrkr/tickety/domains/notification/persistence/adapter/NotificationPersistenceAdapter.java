12af254c3d78d5144388d224090371658481909e
jiwon <kiwijomn@gmail.com>
2025-02-07 18:07:58 +0900

    WRKR-172 :sparkles: Feature: Notification 전체 조회 및 읽음 처리 adapter 레이어 메서드 추가
    <footer>
    #172

--- src/main/java/com/wrkr/tickety/domains/notification/persistence/adapter/NotificationPersistenceAdapter.java
+++ src/main/java/com/wrkr/tickety/domains/notification/persistence/adapter/NotificationPersistenceAdapter.java
@@ -4,6 +4,7 @@ import com.wrkr.tickety.domains.notification.domain.model.Notification;
 import com.wrkr.tickety.domains.notification.persistence.entity.NotificationEntity;
 import com.wrkr.tickety.domains.notification.persistence.mapper.NotificationPersistenceMapper;
 import com.wrkr.tickety.domains.notification.persistence.repository.NotificationRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -18,5 +19,16 @@ public class NotificationPersistenceAdapter {
         NotificationEntity ticketEntity = this.notificationPersistenceMapper.toEntity(notification);
         NotificationEntity savedEntity = this.notificationRepository.save(ticketEntity);
         return this.notificationPersistenceMapper.toDomain(savedEntity);
+    }
+
+    public List<Notification> getAllNotificationsByMemberId(final Long memberId) {
+        List<NotificationEntity> notificationEntities = this.notificationRepository.findAllByMemberId(memberId);
+        return notificationEntities.stream()
+            .map(this.notificationPersistenceMapper::toDomain)
+            .toList();
+    }
+
+    public void updateAllIsReadTrueByMemberId(final Long memberId) {
+        notificationRepository.updateAllIsReadTrueByMemberId(memberId);
     }
 }
