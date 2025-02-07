2dfcf1b149dd28e406eb0389ba75225d558931ba
jiwon <kiwijomn@gmail.com>
2025-02-07 18:06:16 +0900

    WRKR-172 :sparkles: Feature: Notification 전체 조회 및 읽음 처리 query 메서드 구현
    <footer>
    #172

--- src/main/java/com/wrkr/tickety/domains/notification/persistence/repository/NotificationRepository.java
+++ src/main/java/com/wrkr/tickety/domains/notification/persistence/repository/NotificationRepository.java
@@ -1,8 +1,17 @@
 package com.wrkr.tickety.domains.notification.persistence.repository;
 
 import com.wrkr.tickety.domains.notification.persistence.entity.NotificationEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
 
+    List<NotificationEntity> findAllByMemberId(final Long memberId);
+
+    @Modifying
+    @Query("UPDATE NotificationEntity n SET n.isRead = true WHERE n.memberId = :memberId")
+    void updateAllIsReadTrueByMemberId(@Param("memberId") Long memberId);
 }
