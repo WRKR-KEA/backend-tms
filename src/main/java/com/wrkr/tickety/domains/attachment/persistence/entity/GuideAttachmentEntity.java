52cae3143644c2a73d780ccfe084af84cdf5ae9e
PARK SEONWOONG <sunwoong0169@naver.com>
2025-02-10 12:05:52 +0900

    WRKR-219 🐛 Fix: 첨부파일 엔티티 URL 타입 변경

--- src/main/java/com/wrkr/tickety/domains/attachment/persistence/entity/GuideAttachmentEntity.java
+++ src/main/java/com/wrkr/tickety/domains/attachment/persistence/entity/GuideAttachmentEntity.java
@@ -1,9 +1,16 @@
 package com.wrkr.tickety.domains.attachment.persistence.entity;
 
-import com.wrkr.tickety.domains.ticket.domain.model.Guide;
 import com.wrkr.tickety.domains.ticket.persistence.entity.GuideEntity;
 import com.wrkr.tickety.global.entity.BaseTimeEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,7 +34,7 @@ public class GuideAttachmentEntity extends BaseTimeEntity {
     @JoinColumn(name = "guide_id", nullable = false)
     private GuideEntity guide;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String fileUrl;
 
     @Column(nullable = false)
@@ -38,11 +45,11 @@ public class GuideAttachmentEntity extends BaseTimeEntity {
 
     @Builder
     public GuideAttachmentEntity(
-            Long attachmentId,
-            GuideEntity guide,
-            String fileUrl,
-            String fileName,
-            Long fileSize
+        Long attachmentId,
+        GuideEntity guide,
+        String fileUrl,
+        String fileName,
+        Long fileSize
     ) {
         this.attachmentId = attachmentId;
         this.guide = guide;
