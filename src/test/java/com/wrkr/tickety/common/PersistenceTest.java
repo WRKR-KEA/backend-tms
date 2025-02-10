0082e10d94434c9da3b06cd4447188ca0ded53a3
jiwon <kiwijomn@gmail.com>
2025-02-10 14:21:31 +0900

    WRKR-216 :construction: Wip: PersistenceTest 임시 주석 처리

--- src/test/java/com/wrkr/tickety/common/PersistenceTest.java
+++ src/test/java/com/wrkr/tickety/common/PersistenceTest.java
@@ -1,12 +1,8 @@
 package com.wrkr.tickety.common;
 
-import org.junit.jupiter.api.Tag;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@Tag("Persistence")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@DataJpaTest(showSql = false)
-public abstract class PersistenceTest {
-
-}
+//@Tag("Persistence")
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@DataJpaTest(showSql = false)
+//public abstract class PersistenceTest {
+//
+//}
