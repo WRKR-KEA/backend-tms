cdf7951c19f31dba76dd6f2408a2f262ccf5b98b
PARK SEONWOONG <sunwoong0169@naver.com>
2025-02-11 23:41:37 +0900

    WRKR-264 ✅ Test: sql.init.mode : never 수정

--- src/test/java/com/wrkr/tickety/domains/ticket/application/usecase/ticket/ManagerTicketDelegateUseCaseTest.java
+++ src/test/java/com/wrkr/tickety/domains/ticket/application/usecase/ticket/ManagerTicketDelegateUseCaseTest.java
@@ -26,12 +26,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.TestPropertySource;
 
 @ExtendWith(MockitoExtension.class)
-@TestPropertySource(properties = {
-    "spring.liquibase.enabled=false"
-})
 public class ManagerTicketDelegateUseCaseTest extends UnitTest {
 
     private final ManagerTicketDelegateUseCase sut = new ManagerTicketDelegateUseCase(
