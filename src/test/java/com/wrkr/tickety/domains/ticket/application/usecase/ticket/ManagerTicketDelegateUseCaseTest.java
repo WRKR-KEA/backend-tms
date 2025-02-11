4860c7f4f23619fd031216a9253694c639d0f186
jiwon <kiwijomn@gmail.com>
2025-02-11 14:07:52 +0900

    WRKR-265 :bug: Fix: ManagerTicketDelegateUseCaseTest LENIENT 적용

--- src/test/java/com/wrkr/tickety/domains/ticket/application/usecase/ticket/ManagerTicketDelegateUseCaseTest.java
+++ src/test/java/com/wrkr/tickety/domains/ticket/application/usecase/ticket/ManagerTicketDelegateUseCaseTest.java
@@ -23,9 +23,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.context.ApplicationEventPublisher;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class ManagerTicketDelegateUseCaseTest {
 
     @Mock
