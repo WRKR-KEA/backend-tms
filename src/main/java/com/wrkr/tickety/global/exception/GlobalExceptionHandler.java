b67350bc9640ba1804f2dad6288fd6527aa33969
jiwon <kiwijomn@gmail.com>
2025-02-13 15:06:36 +0900

    WRKR-327 :wrench: Chore: HttpMessageNotReadableException 핸들러 추가

--- src/main/java/com/wrkr/tickety/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wrkr/tickety/global/exception/GlobalExceptionHandler.java
@@ -6,11 +6,15 @@ import com.wrkr.tickety.global.response.code.CommonErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,47 +23,58 @@ import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
-
 @Slf4j
 @RestControllerAdvice(annotations = {RestController.class})
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     public ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException e,
-            HttpHeaders headers,
-            HttpStatusCode status,
-            WebRequest request
+        MethodArgumentNotValidException e,
+        HttpHeaders headers,
+        HttpStatusCode status,
+        WebRequest request
     ) {
         Map<String, String> errors = new LinkedHashMap<>();
 
         e.getBindingResult()
-                .getFieldErrors()
-                .forEach(fieldError -> {
-                    String fieldName = fieldError.getField();
-                    String errorMessage = Optional.ofNullable(fieldError.getDefaultMessage()).orElse("");
-                    errors.merge(fieldName, errorMessage,
-                            (existingErrorMessage, newErrorMessage) -> existingErrorMessage + ", " + newErrorMessage);
-                });
+            .getFieldErrors()
+            .forEach(fieldError -> {
+                String fieldName = fieldError.getField();
+                String errorMessage = Optional.ofNullable(fieldError.getDefaultMessage()).orElse("");
+                errors.merge(fieldName, errorMessage,
+                    (existingErrorMessage, newErrorMessage) -> existingErrorMessage + ", " + newErrorMessage);
+            });
 
         return handleExceptionInternalArgs(
-                e,
-                request,
-                errors
+            e,
+            request,
+            errors
         );
     }
 
     @ExceptionHandler
     public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
         String errorMessage = e.getConstraintViolations().stream()
-                .map(ConstraintViolation::getMessage)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("ConstraintViolationException Error"));
+            .map(ConstraintViolation::getMessage)
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("ConstraintViolationException Error"));
 
         return handleExceptionInternalConstraint(e, CommonErrorCode.valueOf(errorMessage), request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+        HttpMessageNotReadableException ex,
+        HttpHeaders headers,
+        HttpStatusCode status,
+        WebRequest request
+    ) {
+        log.error("HttpMessageNotReadableException: {}", ex.getMessage());
+        ApplicationResponse<Void> response = ApplicationResponse.onFailure(
+            CommonErrorCode.METHOD_ARGUMENT_NOT_VALID.getCustomCode(),
+            CommonErrorCode.METHOD_ARGUMENT_NOT_VALID.getMessage()
+        );
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
@@ -67,10 +82,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         log.error("Unexpected error: ", e);
 
         return handleExceptionInternalFalse(
-                e,
-                CommonErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus(),
-                request,
-                e.getMessage()
+            e,
+            CommonErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus(),
+            request,
+            e.getMessage()
         );
     }
 
@@ -82,86 +97,86 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     private ResponseEntity<Object> handleExceptionInternal(
-            ApplicationException e,
-            BaseErrorCode baseErrorCode,
-            HttpHeaders headers,
-            HttpServletRequest request
+        ApplicationException e,
+        BaseErrorCode baseErrorCode,
+        HttpHeaders headers,
+        HttpServletRequest request
     ) {
         ApplicationResponse<Object> body = ApplicationResponse.onFailure(
-                baseErrorCode.getCustomCode(),
-                baseErrorCode.getMessage(),
-                null
+            baseErrorCode.getCustomCode(),
+            baseErrorCode.getMessage(),
+            null
         );
 
         WebRequest webRequest = new ServletWebRequest(request);
 
         return super.handleExceptionInternal(
-                e,
-                body,
-                headers,
-                baseErrorCode.getHttpStatus(),
-                webRequest
+            e,
+            body,
+            headers,
+            baseErrorCode.getHttpStatus(),
+            webRequest
         );
     }
 
     private ResponseEntity<Object> handleExceptionInternalFalse(
-            Exception e,
-            HttpStatus status,
-            WebRequest request,
-            String errorPoint
+        Exception e,
+        HttpStatus status,
+        WebRequest request,
+        String errorPoint
     ) {
         ApplicationResponse<Object> body = ApplicationResponse.onFailure(
-                CommonErrorCode.INTERNAL_SERVER_ERROR.getCustomCode(),
-                CommonErrorCode.INTERNAL_SERVER_ERROR.getMessage(),
-                errorPoint
+            CommonErrorCode.INTERNAL_SERVER_ERROR.getCustomCode(),
+            CommonErrorCode.INTERNAL_SERVER_ERROR.getMessage(),
+            errorPoint
         );
 
         return super.handleExceptionInternal(
-                e,
-                body,
-                HttpHeaders.EMPTY,
-                status,
-                request
+            e,
+            body,
+            HttpHeaders.EMPTY,
+            status,
+            request
         );
     }
 
     private ResponseEntity<Object> handleExceptionInternalArgs(
-            Exception e,
-            WebRequest request,
-            Map<String, String> errorArgs
+        Exception e,
+        WebRequest request,
+        Map<String, String> errorArgs
     ) {
         ApplicationResponse<Object> body = ApplicationResponse.onFailure(
-                CommonErrorCode.BAD_REQUEST.getCustomCode(),
-                CommonErrorCode.BAD_REQUEST.getMessage(),
-                errorArgs
+            CommonErrorCode.BAD_REQUEST.getCustomCode(),
+            CommonErrorCode.BAD_REQUEST.getMessage(),
+            errorArgs
         );
 
         return super.handleExceptionInternal(
-                e,
-                body,
-                HttpHeaders.EMPTY,
-                CommonErrorCode.BAD_REQUEST.getHttpStatus(),
-                request
+            e,
+            body,
+            HttpHeaders.EMPTY,
+            CommonErrorCode.BAD_REQUEST.getHttpStatus(),
+            request
         );
     }
 
     private ResponseEntity<Object> handleExceptionInternalConstraint(
-            Exception e,
-            BaseErrorCode baseErrorCode,
-            WebRequest request
+        Exception e,
+        BaseErrorCode baseErrorCode,
+        WebRequest request
     ) {
         ApplicationResponse<Object> body = ApplicationResponse.onFailure(
-                baseErrorCode.getCustomCode(),
-                baseErrorCode.getMessage(),
-                null
+            baseErrorCode.getCustomCode(),
+            baseErrorCode.getMessage(),
+            null
         );
 
         return super.handleExceptionInternal(
-                e,
-                body,
-                HttpHeaders.EMPTY,
-                baseErrorCode.getHttpStatus(),
-                request
+            e,
+            body,
+            HttpHeaders.EMPTY,
+            baseErrorCode.getHttpStatus(),
+            request
         );
     }
 }
