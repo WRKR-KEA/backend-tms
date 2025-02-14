05e4eb2ea06aa9f1a910af392f366207faf23cbc
PARK SEONWOONG <sunwoong0169@naver.com>
2025-02-14 16:06:49 +0900

    WRKR-342 🐛 Fix: 마이페이지 조회/수정 로직에 캐싱 로직 적용

--- src/main/java/com/wrkr/tickety/domains/member/application/usecase/MyPageInfoUpdateUseCase.java
+++ src/main/java/com/wrkr/tickety/domains/member/application/usecase/MyPageInfoUpdateUseCase.java
@@ -1,7 +1,10 @@
 package com.wrkr.tickety.domains.member.application.usecase;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wrkr.tickety.domains.member.application.dto.request.MyPageInfoUpdateRequest;
 import com.wrkr.tickety.domains.member.application.dto.response.MemberPkResponse;
+import com.wrkr.tickety.domains.member.application.dto.response.MyPageInfoResponse;
+import com.wrkr.tickety.domains.member.application.mapper.MyPageMapper;
 import com.wrkr.tickety.domains.member.domain.model.Member;
 import com.wrkr.tickety.domains.member.domain.service.MemberGetService;
 import com.wrkr.tickety.domains.member.domain.service.MemberUpdateService;
@@ -9,6 +12,7 @@ import com.wrkr.tickety.domains.member.exception.MemberErrorCode;
 import com.wrkr.tickety.global.annotation.architecture.UseCase;
 import com.wrkr.tickety.global.exception.ApplicationException;
 import com.wrkr.tickety.global.utils.PkCrypto;
+import com.wrkr.tickety.infrastructure.redis.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +23,8 @@ public class MyPageInfoUpdateUseCase {
 
     private final MemberGetService memberGetService;
     private final MemberUpdateService memberUpdateService;
+    private final RedisService redisService;
+    private final ObjectMapper objectMapper;
 
     public MemberPkResponse updateMyPageInfo(Long memberId, MyPageInfoUpdateRequest request) {
         Member member = memberGetService.byMemberId(memberId);
@@ -32,10 +38,21 @@ public class MyPageInfoUpdateUseCase {
         }
 
         member.modifyMyPageInfo(request);
-        Member modifieddMember = memberUpdateService.modifyMemberInfo(member);
+        Member modifiedMember = memberUpdateService.modifyMemberInfo(member);
+
+        String key = "MEMBER_INFO:" + memberId;
+        redisService.deleteValues(key);
+
+        try {
+            MyPageInfoResponse updatedResponse = MyPageMapper.toMyPageInfoResponse(modifiedMember);
+            String jsonData = objectMapper.writeValueAsString(updatedResponse);
+            redisService.setValuesWithoutTTL(key, jsonData);
+        } catch (Exception e) {
+            throw new RuntimeException("회원 정보 캐싱 중 오류 발생", e);
+        }
 
         return MemberPkResponse.builder()
-            .memberId(PkCrypto.encrypt(modifieddMember.getMemberId()))
+            .memberId(PkCrypto.encrypt(modifiedMember.getMemberId()))
             .build();
     }
 }
