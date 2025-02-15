2736cdbcdb51ff219fe4ade78ff6d75e69de4c32
qlfyd123 <86708481+qlfyd123@users.noreply.github.com>
2025-02-15 21:48:27 +0900

    WRKR-271 ✅ Test: 2차 카테고리 통계 조회 컨트롤러 테스트코드 작성

--- src/test/java/com/wrkr/tickety/domains/ticket/presentation/TicketStatisticsControllerTest.java
+++ src/test/java/com/wrkr/tickety/domains/ticket/presentation/TicketStatisticsControllerTest.java
@@ -3,6 +3,7 @@ package com.wrkr.tickety.domains.ticket.presentation;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,10 +19,12 @@ import com.wrkr.tickety.domains.ticket.application.usecase.statistics.Statistics
 import com.wrkr.tickety.domains.ticket.domain.constant.StatisticsType;
 import com.wrkr.tickety.global.annotation.WithMockCustomUser;
 import com.wrkr.tickety.global.config.security.jwt.JwtUtils;
+import com.wrkr.tickety.global.utils.PkCrypto;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -133,6 +136,74 @@ class TicketStatisticsControllerTest {
             //then
             mockMvc.perform(
                     post("/api/manager/statistics/{statisticsType}", StatisticsType.DAILY.toString())
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString("{date:invalidDate}")))
+                .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Nested
+    class statisticsByChildCategoryTest {
+
+        @BeforeAll
+        static void init() {
+            PkCrypto pkCrypto = new PkCrypto("AES", "1234567890123456");
+            pkCrypto.init();
+        }
+
+        @Test
+        @DisplayName("2차 카테고리별 통계 조회")
+        @WithMockCustomUser(username = "manager", role = Role.MANAGER, nickname = "managr.hjw")
+        void getChildCategoryStatisticsInDaily() throws Exception {
+            //given
+            LocalDate requestDate = LocalDate.now();
+            String parentCategoryId = "LvI-mc4SwqpS3CixT1m_GQ";
+            StatisticsByCategoryRequest request = StatisticsByCategoryRequest.builder().date(LocalDate.now()).build();
+            List<TicketCount> CategoryTicketCount = new ArrayList<>();
+            IntStream.range(0, 5)
+                .forEach(i -> CategoryTicketCount.add(TicketCount.builder().categoryId("categoryId" + i).count(i).categoryName("categoryName" + i).build()));
+
+            StatisticsByCategoryResponse expectedResponse = StatisticsByCategoryResponse.builder().parentCategoryId("LvI-mc4SwqpS3CixT1m_GQ")
+                .statisticData(StatisticData.builder().firstCategoryTicketCount(CategoryTicketCount).build()).date(requestDate.toString()).build();
+            //when
+            when(statisticsByChildCategoryUseCase.getStatisticsByCategory(StatisticsType.MONTHLY, requestDate, PkCrypto.decrypt(parentCategoryId))).thenReturn(expectedResponse);
+            //when & then
+            mockMvc.perform(
+                    post("/api/manager/statistics/{statisticsType}/{parentCategoryId}",
+                         StatisticsType.MONTHLY, parentCategoryId)
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.date").value(requestDate.toString()));
+        }
+
+        @Test
+        @DisplayName("지원되지 않는 통계 타입은 예외 발생")
+        @WithMockCustomUser(username = "manager", role = Role.MANAGER, nickname = "manager.hjw")
+        void throwExceptionWhenUnsupportedStatisticsType() throws Exception {
+            final String UN_SUPPORTED_STATISTICS_TYPE = "unsupportedStatisticsType";
+            //given
+            StatisticsByCategoryRequest request = StatisticsByCategoryRequest.builder().date(LocalDate.now()).build();
+            //then
+            mockMvc.perform(
+                    post("/api/manager/statistics/{statisticsType}/{parentCategoryId}",
+                         UN_SUPPORTED_STATISTICS_TYPE, "LvI-mc4SwqpS3CixT1m_GQ")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("날짜 형식이 잘못된 경우 예외 발생")
+        @WithMockCustomUser(username = "manager", role = Role.MANAGER, nickname = "manager.hjw")
+        void throwExceptionWhenInvalidDateFormat() throws Exception {
+            //then
+            mockMvc.perform(
+                    post("/api/manager/statistics/{statisticsType}/{parentCategoryId}",
+                         StatisticsType.DAILY, "LvI-mc4SwqpS3CixT1m_GQ")
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString("{date:invalidDate}")))
