52e78571e8a4808f7a3b38ec8c5db4656abc8900
jiwon <kiwijomn@gmail.com>
2025-02-17 03:40:29 +0900

    WRKR-129 :white_check_mark: Test: 담당자 티켓 승인 Controller 레이어 테스트 코드 작성

--- src/test/java/com/wrkr/tickety/domains/ticket/presentation/ManagerTicketControllerTest.java
+++ src/test/java/com/wrkr/tickety/domains/ticket/presentation/ManagerTicketControllerTest.java
@@ -2,6 +2,8 @@ package com.wrkr.tickety.domains.ticket.presentation;
 
 import static com.wrkr.tickety.common.fixture.ticket.TicketFixture.TICKET_CANCEL_01;
 import static com.wrkr.tickety.common.fixture.ticket.TicketFixture.TICKET_COMPLETE_01;
+import static com.wrkr.tickety.common.fixture.ticket.TicketFixture.TICKET_REQUEST_01;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyLong;
@@ -16,6 +18,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -25,6 +28,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wrkr.tickety.domains.auth.exception.AuthErrorCode;
 import com.wrkr.tickety.domains.member.domain.constant.Role;
 import com.wrkr.tickety.domains.member.domain.model.Member;
+import com.wrkr.tickety.domains.member.exception.MemberErrorCode;
 import com.wrkr.tickety.domains.ticket.application.dto.request.ticket.TicketDelegateRequest;
 import com.wrkr.tickety.domains.ticket.application.dto.response.TicketPkResponse;
 import com.wrkr.tickety.domains.ticket.application.usecase.ticket.DepartmentTicketAllGetUseCase;
@@ -44,6 +48,7 @@ import com.wrkr.tickety.global.config.security.jwt.JwtUtils;
 import com.wrkr.tickety.global.exception.ApplicationException;
 import com.wrkr.tickety.global.utils.PkCrypto;
 import com.wrkr.tickety.global.utils.excel.ExcelUtil;
+import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -416,6 +421,231 @@ class ManagerTicketControllerTest {
                         preprocessResponse(prettyPrint()),
                         pathParameters(
                             parameterWithName("ticketId").description("완료할 티켓 ID(PK)")
+                        ),
+                        responseFields(
+                            fieldWithPath("isSuccess").description("성공 여부"),
+                            fieldWithPath("code").description("커스텀 예외 코드"),
+                            fieldWithPath("message").description("예외 메시지")
+                        )
+                    )
+                );
+        }
+    }
+
+    @Nested
+    @DisplayName("담당자 티켓 승인 API [PATCH /api/manager/tickets/approve?ticketId=abc123&ticketId=def456]")
+    class approveTicket {
+
+        @Test
+        @DisplayName("티켓 승인 처리에 성공한다.")
+        @WithMockCustomUser(username = "manager", role = Role.MANAGER, nickname = "manager.kjw", memberId = 2L)
+        void approveTicketSuccess() throws Exception {
+            // given
+            final List<TicketPkResponse> response = List.of(new TicketPkResponse(PkCrypto.encrypt(TICKET_ID)));
+            doReturn(response).when(ticketApproveUseCase).approveTicket(anyLong(), anyList());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                .patch("/api/manager/tickets/approve")
+                .queryParam("ticketId", PkCrypto.encrypt(TICKET_ID))
+                //.header(AUTHORIZATION, BEARER_TOKEN + " " + ACCESS_TOKEN)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON);
+
+            mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andDo(
+                    document(
+                        "ManagerTicketApi/Approve/Success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+//                        requestHeaders(
+//                            headerWithName(AUTHORIZATION).description("Access Token")
+//                        ),
+                        queryParameters(
+                            parameterWithName("ticketId").description("승인할 티켓 ID(PK) 리스트")
+                        ),
+                        responseFields(
+                            fieldWithPath("isSuccess").description("성공 여부"),
+                            fieldWithPath("code").description("커스텀 예외 코드"),
+                            fieldWithPath("message").description("예외 메시지"),
+                            fieldWithPath("result").description("반환 결과"),
+                            fieldWithPath("result[].ticketId").description("승인된 티켓의 ID(PK) 리스트")
+                        )
+                    )
+                );
+        }
+
+        @Test
+        @DisplayName("Authorization Header에 AccessToken이 없으면 티켓 승인 처리에 실패한다.")
+//        @WithMockCustomUser(username = "manager", role = Role.MANAGER, nickname = "manager.kjw", memberId = 2L)
+        void withoutAccessToken() throws Exception {
+            // given
+            final List<TicketPkResponse> response = List.of(new TicketPkResponse(PkCrypto.encrypt(TICKET_ID)));
+            doReturn(response).when(ticketApproveUseCase).approveTicket(anyLong(), anyList());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                .patch("/api/manager/tickets/approve")
+                .queryParam("ticketId", PkCrypto.encrypt(TICKET_ID))
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON);
+
+            // then
+            final AuthErrorCode expectedError = AuthErrorCode.AUTHENTICATION_FAILED;
+            mockMvc.perform(requestBuilder)
+                .andExpect(
+                    status().isUnauthorized()
+                )
+                .andDo(
+                    document(
+                        "ManagerTicketApi/Approve/Failure/Case1",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(
+                            parameterWithName("ticketId").description("승인할 티켓 ID(PK) 리스트")
+                        )
+//                        responseFields(
+//                            fieldWithPath("isSuccess").description("성공 여부"),
+//                            fieldWithPath("code").description("커스텀 예외 코드"),
+//                            fieldWithPath("message").description("예외 메시지")
+//                        )
+                    )
+                );
+        }
+
+        @Test
+        @DisplayName("승인하는 회원의 권한이 담당자가 아니면 티켓 승인 처리에 실패한다.")
+        @WithMockCustomUser(username = "user", role = Role.USER, nickname = "user.kjw", memberId = 2L)
+        void throwExceptionByMemberNotAllowed() throws Exception {
+            // given
+            Ticket ticket = TICKET_REQUEST_01.toRequestTicket();
+
+            doThrow(ApplicationException.from(MemberErrorCode.MEMBER_NOT_ALLOWED))
+                .when(ticketApproveUseCase)
+                .approveTicket(anyLong(), anyList());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                .patch("/api/manager/tickets/approve")
+                .queryParam("ticketId", PkCrypto.encrypt(ticket.getTicketId()))
+                //.header(AUTHORIZATION, BEARER_TOKEN + " " + ACCESS_TOKEN)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON);
+
+            // then
+            final MemberErrorCode expectedError = MemberErrorCode.MEMBER_NOT_ALLOWED;
+            mockMvc.perform(requestBuilder)
+                .andExpectAll(
+                    status().isForbidden(),
+                    jsonPath("$.isSuccess").exists(),
+                    jsonPath("$.isSuccess").value(false),
+                    jsonPath("$.code").exists(),
+                    jsonPath("$.code").value(expectedError.getCustomCode()),
+                    jsonPath("$.message").exists(),
+                    jsonPath("$.message").value(expectedError.getMessage())
+                )
+                .andDo(
+                    document(
+                        "ManagerTicketApi/Approve/Failure/Case2",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(
+                            parameterWithName("ticketId").description("승인할 티켓 ID(PK) 리스트")
+                        ),
+                        responseFields(
+                            fieldWithPath("isSuccess").description("성공 여부"),
+                            fieldWithPath("code").description("커스텀 예외 코드"),
+                            fieldWithPath("message").description("예외 메시지")
+                        )
+                    )
+                );
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 티켓이면 티켓 승인 처리에 실패한다.")
+        @WithMockCustomUser(username = "manager", role = Role.MANAGER, nickname = "manager.kjw", memberId = 2L)
+        void throwExceptionByTicketNotFound() throws Exception {
+            // given
+            doThrow(ApplicationException.from(TicketErrorCode.TICKET_NOT_FOUND))
+                .when(ticketApproveUseCase)
+                .approveTicket(anyLong(), anyList());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                .patch("/api/manager/tickets/approve")
+                .queryParam("ticketId", PkCrypto.encrypt(TICKET_ID))
+                //.header(AUTHORIZATION, BEARER_TOKEN + " " + ACCESS_TOKEN)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON);
+
+            // then
+            final TicketErrorCode expectedError = TicketErrorCode.TICKET_NOT_FOUND;
+            mockMvc.perform(requestBuilder)
+                .andExpectAll(
+                    status().isNotFound(),
+                    jsonPath("$.isSuccess").exists(),
+                    jsonPath("$.isSuccess").value(false),
+                    jsonPath("$.code").exists(),
+                    jsonPath("$.code").value(expectedError.getCustomCode()),
+                    jsonPath("$.message").exists(),
+                    jsonPath("$.message").value(expectedError.getMessage())
+                )
+                .andDo(
+                    document(
+                        "ManagerTicketApi/Approve/Failure/Case3",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(
+                            parameterWithName("ticketId").description("승인할 티켓 ID(PK) 리스트")
+                        ),
+                        responseFields(
+                            fieldWithPath("isSuccess").description("성공 여부"),
+                            fieldWithPath("code").description("커스텀 예외 코드"),
+                            fieldWithPath("message").description("예외 메시지")
+                        )
+                    )
+                );
+        }
+
+        @Test
+        @DisplayName("요청 상태인 티켓이 아니면 티켓 승인 처리에 실패한다.")
+        @WithMockCustomUser(username = "manager", role = Role.MANAGER, nickname = "manager.kjw", memberId = 2L)
+        void throwExceptionByTicketNotApprovable() throws Exception {
+            // given
+            Ticket ticket = TICKET_COMPLETE_01.toInProgressTicket();
+
+            doThrow(ApplicationException.from(TicketErrorCode.TICKET_NOT_APPROVABLE))
+                .when(ticketApproveUseCase)
+                .approveTicket(anyLong(), anyList());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                .patch("/api/manager/tickets/approve")
+                .queryParam("ticketId", PkCrypto.encrypt(ticket.getTicketId()))
+                //.header(AUTHORIZATION, BEARER_TOKEN + " " + ACCESS_TOKEN)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON);
+
+            // then
+            final TicketErrorCode expectedError = TicketErrorCode.TICKET_NOT_APPROVABLE;
+            mockMvc.perform(requestBuilder)
+                .andExpectAll(
+                    status().isConflict(),
+                    jsonPath("$.isSuccess").exists(),
+                    jsonPath("$.isSuccess").value(false),
+                    jsonPath("$.code").exists(),
+                    jsonPath("$.code").value(expectedError.getCustomCode()),
+                    jsonPath("$.message").exists(),
+                    jsonPath("$.message").value(expectedError.getMessage())
+                )
+                .andDo(
+                    document(
+                        "ManagerTicketApi/Approve/Failure/Case4",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(
+                            parameterWithName("ticketId").description("승인할 티켓 ID(PK) 리스트")
                         ),
                         responseFields(
                             fieldWithPath("isSuccess").description("성공 여부"),
