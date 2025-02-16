a3a7a166e14c8ca8310c6c1c16865804752ed49b
jiwon <kiwijomn@gmail.com>
2025-02-16 22:09:47 +0900

    WRKR-129 :recycle: Refactor: 티켓 승인 OptimisticLockException 예외 처리 추가

--- src/main/java/com/wrkr/tickety/domains/ticket/domain/service/ticket/TicketUpdateService.java
+++ src/main/java/com/wrkr/tickety/domains/ticket/domain/service/ticket/TicketUpdateService.java
@@ -3,7 +3,10 @@ package com.wrkr.tickety.domains.ticket.domain.service.ticket;
 import com.wrkr.tickety.domains.member.domain.model.Member;
 import com.wrkr.tickety.domains.ticket.domain.constant.TicketStatus;
 import com.wrkr.tickety.domains.ticket.domain.model.Ticket;
+import com.wrkr.tickety.domains.ticket.exception.TicketErrorCode;
 import com.wrkr.tickety.domains.ticket.persistence.adapter.TicketPersistenceAdapter;
+import com.wrkr.tickety.global.exception.ApplicationException;
+import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,8 +24,12 @@ public class TicketUpdateService {
     }
 
     public Ticket approveTicket(Ticket ticket, Member member) {
-        ticket.approveTicket(member);
-        return ticketPersistenceAdapter.save(ticket);
+        try {
+            ticket.approveTicket(member);
+            return ticketPersistenceAdapter.save(ticket);
+        } catch (OptimisticLockException e) {
+            throw ApplicationException.from(TicketErrorCode.TICKET_NOT_APPROVABLE);
+        }
     }
 
     public Ticket updateManager(Ticket ticket, Member manager) {
