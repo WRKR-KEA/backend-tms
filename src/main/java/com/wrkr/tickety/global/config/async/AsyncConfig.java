a3bc5863145b20f33da9d98c896ef384773a7f7b
Sujin Kang <1214sujin@naver.com>
2025-02-17 17:49:43 +0900

    WRKR-371 :bug: Fix: pool size를 설정 파일에서 설정하도록 수정
    <footer> #355

--- src/main/java/com/wrkr/tickety/global/config/async/AsyncConfig.java
+++ src/main/java/com/wrkr/tickety/global/config/async/AsyncConfig.java
@@ -1,6 +1,7 @@
 package com.wrkr.tickety.global.config.async;
 
 import java.util.concurrent.Executor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
@@ -11,11 +12,16 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class AsyncConfig implements AsyncConfigurer {
 
+    @Value("${thread.corePoolSize}")
+    private int corePoolSize;
+    @Value("${thread.maxPoolSize}")
+    private int maxPoolSize;
+
     @Bean
     public Executor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(30);
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
         return executor;
     }
 
