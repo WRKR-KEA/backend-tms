20022a41e2e6dccf4b0cd1d44e27f0b0395ec1f6
jiwon <kiwijomn@gmail.com>
2025-02-17 20:17:35 +0900

    WRKR-374 :bug: Fix: 일부 API Swagger CustomErrorCodes 명세 수정

--- src/main/java/com/wrkr/tickety/domains/member/presentation/ManagerMemberController.java
+++ src/main/java/com/wrkr/tickety/domains/member/presentation/ManagerMemberController.java
@@ -3,7 +3,6 @@ package com.wrkr.tickety.domains.member.presentation;
 import com.wrkr.tickety.domains.member.application.dto.response.ManagerGetAllManagerResponse;
 import com.wrkr.tickety.domains.member.application.usecase.ManagerGetAllManagersUseCase;
 import com.wrkr.tickety.domains.member.domain.model.Member;
-import com.wrkr.tickety.global.annotation.swagger.CustomErrorCodes;
 import com.wrkr.tickety.global.common.dto.ApplicationPageRequest;
 import com.wrkr.tickety.global.common.dto.ApplicationPageResponse;
 import com.wrkr.tickety.global.response.ApplicationResponse;
@@ -25,7 +24,6 @@ public class ManagerMemberController {
     private final ManagerGetAllManagersUseCase managerGetAllManagersUseCase;
 
     @Operation(summary = "담당자 - 모든 담당자 조회", description = "모든 담당자를 조회합니다.")
-    @CustomErrorCodes({})
     @GetMapping
     public ApplicationResponse<ManagerGetAllManagerResponse> getAllManagers(
         @AuthenticationPrincipal Member member
@@ -35,7 +33,6 @@ public class ManagerMemberController {
     }
 
     @Operation(summary = "담당자 - 모든 담당자 조회(페이징)", description = "모든 담당자를 페이징하여 조회합니다.")
-    @CustomErrorCodes({})
     @GetMapping("/page")
     public ApplicationResponse<ApplicationPageResponse<ManagerGetAllManagerResponse.Managers>> getAllPageManagers(
         @Parameter(description = "페이징", example = "{\"page\":1,\"size\":20}")
