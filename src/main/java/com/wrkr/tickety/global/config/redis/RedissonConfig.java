a5ac43e95309d758221b1bdd65fc65c0d80a10f1
hurrayPersimmon <rahel337@naver.com>
2025-02-18 04:32:42 +0900

    WRKR-393 :wrench: Chore: Redis 분산 락 Lib Redisson 설치

---
+++ src/main/java/com/wrkr/tickety/global/config/redis/RedissonConfig.java
@@ -0,0 +1,31 @@
+package com.wrkr.tickety.global.config.redis;
+
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        String redisAddress = "redis://" + host + ":" + port;
+        config.useSingleServer().setAddress(redisAddress).setPassword(password);
+        config.setCodec(new StringCodec());
+        return org.redisson.Redisson.create(config);
+    }
+}
