3459834580002178dde5dc72411e8b59d634e614
Sujin Kang <1214sujin@naver.com>
2025-02-18 20:33:49 +0900

    WRKR-410 :bug: Fix: 알림 중복 저장 문제 해결
    <footer> #407

--- src/main/java/com/wrkr/tickety/domains/notification/domain/service/application/SseEmitterService.java
+++ src/main/java/com/wrkr/tickety/domains/notification/domain/service/application/SseEmitterService.java
@@ -1,10 +1,7 @@
 package com.wrkr.tickety.domains.notification.domain.service.application;
 
-import static com.wrkr.tickety.domains.notification.application.mapper.NotificationMapper.toNotification;
-
 import com.wrkr.tickety.domains.member.domain.model.Member;
 import com.wrkr.tickety.domains.notification.domain.constant.NotificationType;
-import com.wrkr.tickety.domains.notification.domain.model.Notification;
 import com.wrkr.tickety.domains.notification.persistence.adapter.NotificationPersistenceAdapter;
 import com.wrkr.tickety.domains.notification.persistence.repository.EmitterRepository;
 import java.io.IOException;
@@ -24,7 +21,7 @@ public class SseEmitterService {
 
     private static final Long DEFAULT_TIMEOUT = 60L * 1000;
 
-    public SseEmitter subscribe(Long memberId, String lastEventId) {
+    public SseEmitter subscribe(Long memberId) {
         String emitterId = memberId + "_" + System.currentTimeMillis();
         SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
 
@@ -34,40 +31,18 @@ public class SseEmitterService {
 
         sendToClient(emitter, emitterId, "connect", "Subscribe EventStream: [memberId = " + memberId + "]");
 
-        if (!lastEventId.isEmpty()) {
-            Map<String, Object> events = emitterRepository.findAllEventCacheStartWithByMemberId(String.valueOf(memberId));
-            events.entrySet().stream()
-                .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
-                .forEach(entry -> sendToClient(emitter, entry.getKey(), entry.getValue()));
-        }
-
         return emitter;
     }
 
     public void send(Member member, NotificationType notificationType, String content) {
-        Notification notification = notificationPersistenceAdapter.save(
-            toNotification(member.getMemberId(), member.getProfileImage(), notificationType, content)
-        );
         String memberId = String.valueOf(member.getMemberId());
 
         Map<String, SseEmitter> sseEmitters = emitterRepository.findAllEmitterStartWithByMemberId(memberId);
         sseEmitters.forEach(
             (key, emitter) -> {
-                emitterRepository.saveEventCache(key, notification);
                 sendToClient(emitter, key, notificationType.name().toLowerCase(), content);
             }
         );
-    }
-
-    private void sendToClient(SseEmitter emitter, String emitterId, Object data) {
-        try {
-            emitter.send(SseEmitter.event()
-                .id(emitterId)
-                .data(data));
-        } catch (IOException exception) {
-            emitter.complete();
-            log.warn("[SSE] Failed to send event: {}: {}", emitterId, exception.getMessage());
-        }
     }
 
     private void sendToClient(SseEmitter emitter, String emitterId, String type, Object data) {
